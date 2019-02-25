a7fd15cfa3e37afdd9633ec7559850a06bed0893
Geoffrey Copin <copin@adacore.com>
2019-02-25 11:18:11 +0100

    Edit NodeList to use Ada nodes instead of LKQL nodes

--- src/interpreter-types-nodelists.ads
+++ src/interpreter-types-nodelists.ads
@@ -1,11 +1,11 @@
 with Ada.Containers.Vectors;
-with Liblkqllang.Analysis; use Liblkqllang.Analysis;
+with Libadalang.Analysis; use Libadalang.Analysis;
 
 package Interpreter.Types.NodeLists is
 
    package Nodes_Vector is new
      Ada.Containers.Vectors (Index_Type   => Natural,
-                             Element_Type => LEL.LKQL_Node);
+                             Element_Type => LAL.Ada_Node);
    use Nodes_Vector;
 
    type NodeList is record
