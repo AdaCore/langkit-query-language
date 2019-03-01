24f356e2495b8182f8d0129db4d42251abffc7c9
Geoffrey Copin <copin@adacore.com>
2019-03-01 11:09:27 +0100

    Add test cases for interpreter errors

--- testsuite/drivers/interpreter.py
+++ testsuite/drivers/interpreter.py
@@ -15,8 +15,6 @@ def get_interpreter_path():
 
 
 ADA_PROJECTS_PATH = os.path.join(os.path.dirname(os.getcwd()), 'testsuite', 'ada_projects')
-# Will be used if the test case doesn't provide a `project` value
-DEFAULT_PROJECT = os.path.join(ADA_PROJECTS_PATH, 'default_project', 'default.gpr')
 
 INTERPRETER_PATH = get_interpreter_path()
 
@@ -28,23 +26,24 @@ class InterpreterDriver(BasicTestDriver):
 
         expected = read_to_string(os.path.join(test_dir, 'output'))
         status = TestStatus.PASS
-        failure_expected = 'outcome' in self.test_env and self.test_env['outcome'] == 'failure'
-
-        if 'project' not in self.test_env:
-            project_path = DEFAULT_PROJECT
-        else:
+        failure_expected = 'failure' in self.test_env and self.test_env['failure'] == 'true'
+        if self.test_env['project']:
             project_path = os.path.join(ADA_PROJECTS_PATH, self.test_env['project'])
+        else:
+            project_path = ''
+        args = [a for a in [INTERPRETER_PATH, script_path, project_path] if a != '']
 
-        process = Run([INTERPRETER_PATH, script_path, project_path])
+        process = Run(args)
         process_output = remove_paths(process.out)
+        process_failure = process.status != 0
 
-        if process.err is not None and not failure_expected:
+        if process_failure != failure_expected:
             status = TestStatus.ERROR
         elif process_output != expected:
             status = TestStatus.FAIL
 
         self.result.set_status(status)
-        self.result.env['output'] = process.out
+        self.result.env['output'] = process_output
         self.result.env['expected'] = expected
         self.push_result()
 
