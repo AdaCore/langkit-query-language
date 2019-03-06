a090ade3948638415a6bf548c68482382061bbb6
Geoffrey Copin <copin@adacore.com>
2019-03-06 17:32:56 +0100

    Compile the interpreter before launching the testsuite

--- testsuite/drivers/util.py
+++ testsuite/drivers/util.py
@@ -1,3 +1,36 @@
+from e3.fs import mkdir
+from e3.os.process import Run
+import os
+import sys
+
+TESTSUITE_ROOT_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+INTERPRETER_PATH = os.path.join(TESTSUITE_ROOT_DIR, 'build', 'obj', 'main')
+
+
+def make_interpreter(gcov=False):
+    print "Compiling the interpreter (gcov=%s)" % gcov
+    build_dir = os.path.join(TESTSUITE_ROOT_DIR, 'build')
+    mkdir(build_dir)
+
+    # Compute gprbuild invocation
+    gprbuild = [
+        'gprbuild', os.path.join('..', '..', 'lkql_interpreter.gpr'),
+        '--relocate-build-tree=%s' % build_dir, '-j0'
+    ]
+
+    if gcov:
+        gprbuild.extend([
+            '-cargs', '-fprofile-arcs', '-ftest-coverage', '-gargs',
+            '-largs', '-fprofile-arcs', '-ftest-coverage'
+        ])
+
+    p = Run(gprbuild, cwd=build_dir, output=sys.stdout)
+    assert p.status == 0, ("interperter build failed:\n%s" % p.out)
+
+
 def read_to_string(path):
     with open(path, 'r') as f:
         text = f.read()
--- testsuite/testsuite.py
+++ testsuite/testsuite.py
@@ -1,7 +1,12 @@
 from e3.testsuite import Testsuite
-from drivers import ParserDriver, InterpreterDriver
+from drivers import ParserDriver, InterpreterDriver, make_interpreter
 import os
-import subprocess
+
+TESTSUITE_ROOT_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+INTERPRETER_PATH = os.path.join(TESTSUITE_ROOT_DIR, 'build', 'obj', 'main')
 
 INTERPRETER_GPR_FILE = os.path.join(os.path.dirname(os.getcwd()), 'lkql_interpreter.gpr')
 
@@ -12,9 +17,9 @@ class LKQLTestsuite(Testsuite):
 
 
 if __name__ == "__main__":
+    make_interpreter(True)
     suite = LKQLTestsuite(os.path.dirname(__file__))
     suite.testsuite_main()
     for k, v in suite.test_status_counters.iteritems():
         if v != 0:
             print "%s: %d" % (k, v)
-    subprocess.call(["dot", "-Tpng", "out/new/tests.dot", "-o", "out/out.png"])
