29324627a01d812c7e5bebde171d39059398e490
Geoffrey Copin <copin@adacore.com>
2019-03-08 17:37:14 +0100

    Extend testsuite

--- testsuite/ada_projects/default_project/src/main.adb
+++ testsuite/ada_projects/default_project/src/main.adb
@@ -1,7 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
-   Message  : String := "Hello World !";
+   Message         : String := "Hello World !";
+   ConstantMessage : constant String := "Hello Constant world !";
 begin
    Put_Line (Message);
 end Main;
