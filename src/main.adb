3dc98e3b1fb30af79fb58d2c610564826ab0c00c
Geoffrey Copin <copin@adacore.com>
2019-03-11 17:08:00 +0100

    Make project argument optionnal and add --auto_dirs option

--- src/main.adb
+++ src/main.adb
@@ -1,7 +1,11 @@
 with Run;
 
+with Libadalang.Analysis;      use Libadalang.Analysis;
+with Libadalang.Auto_Provider; use Libadalang.Auto_Provider;
+
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
+with GNATCOLL.VFS;       use GNATCOLL.VFS;
 with GNATCOLL.Opt_Parse; use GNATCOLL.Opt_Parse;
 
 --  Usage:
@@ -30,11 +34,23 @@ procedure Main is
          Arg_Type => Unbounded_String,
          Help     => "Path of the LKQL script to evaluate");
 
-      package Project_Path is new Parse_Positional_Arg
-        (Parser   => Parser,
-         Name     => "project_path",
+      package Project_Path is new Parse_Option
+        (Parser      => Parser,
+         Long        => "project",
+         Short       => "-p",
+         Arg_Type    => Unbounded_String,
+         Default_Val => Null_Unbounded_String,
+         Help        => "Path of the GPR project file");
+
+      package Auto_Dirs is new Parse_Option_List
+        (Parser => Parser,
+         Short  => "-a",
+         Long   => "auto_dirs",
          Arg_Type => Unbounded_String,
-         Help     => "Path of the GPR project file");
+         Accumulate => True,
+         Help       =>
+            "Directories to use for the auto provider. If one is passed, "
+            & "auto provider will be used, and project options ignored");
 
       package Recovery_Enabled is new Parse_Flag
         (Parser => Parser,
@@ -45,7 +61,35 @@ procedure Main is
    end Arg;
 
 begin
-   if Arg.Parser.Parse then
+   if not Arg.Parser.Parse then
+      return;
+   end if;
+
+   if Arg.Auto_Dirs.Get'Length > 0 then
+      declare
+         Auto_Dirs : Arg.Auto_Dirs.Result_Array renames Arg.Auto_Dirs.Get;
+         Dirs      : GNATCOLL.VFS.File_Array (Auto_Dirs'Range);
+         Files     : GNATCOLL.VFS.File_Array_Access;
+         UFP       : Unit_Provider_Reference;
+         Context   : Analysis_Context;
+      begin
+         for I in Dirs'Range loop
+            Dirs (I) := Create (+To_String (Auto_Dirs (I)));
+         end loop;
+
+         Files := Find_Files (Directories => Dirs);
+         UFP := Create_Auto_Provider_Reference (Files.all);
+         Context := Create_Context (Unit_Provider => UFP);
+
+         Run.Run_On_Files (To_String (Arg.Script_Path.Get),
+                           Context,
+                           Files,
+                           Arg.Recovery_Enabled.Get);
+
+         GNATCOLL.VFS.Unchecked_Free (Files);
+      end;
+
+   elsif Length (Arg.Project_Path.Get) > 0 then
       Run.Run_Against_Project (To_String (Arg.Script_Path.Get),
                                To_String (Arg.Project_Path.Get),
                                Arg.Recovery_Enabled.Get);
