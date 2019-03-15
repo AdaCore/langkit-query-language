92090e0b0ea2c2fcfe811ac32859ed04f6ec57bd
Geoffrey Copin <copin@adacore.com>
2019-03-15 11:37:48 +0100

    Change the public interface of Interpreter.Evaluation
    Make To_Ada_Node_Kind public and change it's signature to accept bounded strings
    Make Typed_Eval public

--- src/interpreter-evaluation.ads
+++ src/interpreter-evaluation.ads
@@ -1,5 +1,7 @@
+with Interpreter.Primitives;       use Interpreter.Primitives;
 with Interpreter.Eval_Contexts;    use Interpreter.Eval_Contexts;
-with Interpreter.Primitives; use Interpreter.Primitives;
+
+with Langkit_Support.Text; use Langkit_Support.Text;
 
 package Interpreter.Evaluation is
 
@@ -8,5 +10,17 @@ package Interpreter.Evaluation is
    --  Return the result of the AST node's evaluation in the given context.
    --  An Eval_Error will be raised if the node represents an invalid query or
    --  expression.
+
+   function Typed_Eval (Ctx           : in out Eval_Context;
+                        Node          : LEL.LKQL_Node'Class;
+                        Expected_Kind : Primitive_Kind) return Primitive;
+   --  Evaluate the given node and raise an exception if the kind of the
+   --  result doesn't match 'Expected_Kind".
+
+   function To_Ada_Node_Kind
+     (Kind_Name : Text_Type) return LALCO.Ada_Node_Kind_Type;
+   --  Return the Ada_Node_Kind_Type that matches the given name.
+   --  Raise a program error if the name doesn't correspond to any
+   --  ada_Node_Kind_Type.
 
 end Interpreter.Evaluation;
