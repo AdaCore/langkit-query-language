9b0219dc9f45af827bda4736a8c23801d86606cb
Geoffrey Copin <copin@adacore.com>
2019-03-27 17:40:38 +0100

    Add doc

--- src/options.ads
+++ src/options.ads
@@ -1,22 +1,35 @@
 generic
 
    type Value_Type is private;
+   --  Type of the optional value
 
 package Options is
 
-   type Option_Kind is (Kind_None, Kind_Some);
+   type Option_Kind is
+     (Kind_None,
+      --  The Option doesn't contains a value
+      Kind_Some
+      --  The Option contains a value
+     );
 
    type Option (Kind : Option_Kind := Kind_None) is private;
+   --  Stores an optional value
 
    function Is_Some (Self : Option) return Boolean;
+   --  Return True if the option contains a value
 
    function Is_None (Self : Option) return Boolean;
+   --  Return True if the option doesn't contain a value
 
    function Extract (Self : Option) return Value_Type;
+   --  Return the wrapped value, if any.
+   --  A Program_Error will be raised if there is no value.
 
    function To_Option (Value : Value_Type) return Option;
+   --  Create an Option value that contains the given value
 
    function None return Option;
+   --  Create an Option that does'n contain a value
 
 private
 
