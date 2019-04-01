d36801f03661275e0e6aa6b2d1ddd02e87b61c19
Geoffrey Copin <copin@adacore.com>
2019-04-01 16:34:23 +0200

    Add type 'Vec_Iterator' to Iters.Iterators

--- src/iters-iterators.ads
+++ src/iters-iterators.ads
@@ -11,6 +11,13 @@ package Iters.Iterators is
    type Element_Array is array (Positive range <>) of Element_Type;
    --  Array type to use when consuming the iterator into an array of elements
 
+   package Element_Vectors is new Ada.Containers.Vectors
+     (Positive, Element_Type);
+   --  Vectors of Element_Type values
+
+   type Element_Vector_Access is access all Element_Vectors.Vector;
+   --  Pointer to a vector of Element_Type values
+
    --------------
    -- Iterator --
    --------------
@@ -163,6 +170,24 @@ package Iters.Iterators is
    --  Reset the iterator. Further calls to 'Next' will yield the cached
    --  elements.
 
+   ------------------
+   -- Vec_Iterator --
+   ------------------
+
+   type Vec_Iterator is new Iterator_Interface with private;
+   --  Iterator over a vector's elements
+
+   function To_Iterator
+     (Elements : Element_Vectors.Vector) return Vec_Iterator;
+   --  Create a vec_Iterator yielding the elements of the given vector.
+
+   overriding function Next (Iter   : in out Vec_Iterator;
+                             Result : out Element_Type) return Boolean;
+
+   overriding function Clone (Iter : Vec_Iterator) return Vec_Iterator;
+
+   overriding procedure Release (Iter : in out Vec_Iterator);
+
 private
 
    procedure Free_Predicate_Access is new Ada.Unchecked_Deallocation
@@ -191,6 +216,11 @@ private
       Cache_Pos : Cache_Index := -1;
       --  Index of the next cache value to read.
       --  -1 if the wrapped iterator hasn't been fully consumed yet
+   end record;
+
+   type Vec_Iterator is new Iterator_Interface with record
+      Elements     : Element_Vector_Access;
+      Next_Element : Positive;
    end record;
 
 end Iters.Iterators;
