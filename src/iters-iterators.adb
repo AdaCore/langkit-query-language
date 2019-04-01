dd8364cdc12f1cc0358c84a28e1f53ebf38c1e2f
Geoffrey Copin <copin@adacore.com>
2019-04-01 16:33:01 +0200

    Add type 'Ada_Func_Wrapper' to Iters.Iterators.Funcs

--- src/iters-iterators.adb
+++ src/iters-iterators.adb
@@ -197,4 +197,37 @@ package body Iters.Iterators is
       Iter.Cache_Pos := 1;
    end Reset;
 
+   package body Funcs is
+
+      --------------
+      -- Evaluate --
+      --------------
+
+      function Evaluate (Self    : in out Ada_Func_Wrapper;
+                      Element : Element_Type) return Return_Type
+      is
+      begin
+         return Self.Fn (Element);
+      end Evaluate;
+
+      -----------
+      -- Clone --
+      -----------
+
+      function Clone (Self : Ada_Func_Wrapper) return Ada_Func_Wrapper is
+      begin
+         return Ada_Func_Wrapper'(Fn => Self.Fn);
+      end Clone;
+
+      -------------
+      -- To_Func --
+      -------------
+
+      function To_Func (Fn : Ada_Func_Access) return Ada_Func_Wrapper is
+      begin
+         return Ada_Func_Wrapper'(Fn => Fn);
+      end To_Func;
+
+   end Funcs;
+
 end Iters.Iterators;
