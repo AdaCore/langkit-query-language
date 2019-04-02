b07f98dd72f8dccce226675d550c745da5c43fbf
Geoffrey Copin <copin@adacore.com>
2019-04-02 15:11:01 +0200

    Refactor Iter.Maps
    To take a second package parameter representing the output iterator type

--- src/iters-maps.ads
+++ src/iters-maps.ads
@@ -6,18 +6,22 @@ generic
    with package Input_Iterators is new Iters.Iterators (<>);
    --  Wrapped iterators
 
-   type Result_Type is private;
-   --  Type of values returns by the mapping function
+   with package Output_Iterators is new Iters.Iterators (<>);
+   --  Wrapped iterators
 
 package Iters.Maps is
 
-   package Map_Iterators is new Iters.Iterators (Result_Type);
+   subtype Result_Type is Output_Iterators.Element_Type;
 
    package Map_Funcs is new Funcs (Input_Iterators.Element_Type, Result_Type);
    --  Abstraction representing a function that takes values from the input
    --  iterator and returns values of type Return_Type.
 
-   type Map_Iter is new Map_Iterators.Iterator_Interface with private;
+   subtype Map_Func is Map_Funcs.Func;
+
+   package Predicates renames Output_Iterators.Predicates;
+
+   type Map_Iter is new Output_Iterators.Iterator_Interface with private;
    --  Iterator that maps a function over the elements of a given iterator
 
    overriding function Next (Iter   : in out Map_Iter;
@@ -35,7 +39,7 @@ package Iters.Maps is
 
 private
 
-   type Map_Iter is new Map_Iterators.Iterator_Interface with record
+   type Map_Iter is new Output_Iterators.Iterator_Interface with record
       Inner : Input_Iterators.Iterator_Access;
       Fn    : Map_Funcs.Func_Access;
    end record;
