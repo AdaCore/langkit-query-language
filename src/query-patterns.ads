bb5798365fb8b8045ae2b8f1e414076fc9e676b2
Geoffrey Copin <copin@adacore.com>
2019-04-04 11:27:24 +0200

    Add documentation to Query.Patterns

--- src/query-patterns.ads
+++ src/query-patterns.ads
@@ -4,8 +4,10 @@ with Interpreter.Eval_Contexts; use Interpreter.Eval_Contexts;
 package Query.Patterns is
 
    subtype Iterator_Predicate_Interface is Node_Iterators.Predicates.Func;
+   --  Predicates on 'Iterator_Node' values
 
    subtype Iterator_Predicate_Access is Node_Iterators.Predicate_Access;
+   --  Pointer to a predicate on 'Iterator_Node' values
 
    function Make_Query_Iterator (Ctx  : Eval_Context_Ptr;
                                  Node : LEL.Query)
@@ -14,6 +16,10 @@ package Query.Patterns is
    --  belong to the result of the given query.
 
 private
+
+   ---------------------
+   -- Query_Predicate --
+   ---------------------
 
    type Query_Predicate is new Iterator_Predicate_Interface with record
       Ctx   : Eval_Context_Ptr;
@@ -38,6 +44,10 @@ private
      (Self : Query_Predicate) return Query_Predicate;
    --  Return a copy of the given Query_Predicate
 
+   --------------
+   -- Matching --
+   --------------
+
    function Match_Unfiltered_Query (Ctx   : Eval_Context_Ptr;
                                     Query : LEL.Query;
                                     Node  : Iterator_Node) return Boolean;
@@ -52,8 +62,11 @@ private
 
    type Match is record
       Success : Boolean;
+      --  True if the match attempt succeeded
       Bindings : String_Value_Maps.Map;
+      --  Stores the bindings specified in the matched pattern, if any
    end record;
+   --  Represents the result of a match attempt
 
    Match_Failure : constant Match :=
      (Success => False, Bindings => String_Value_Maps.Empty_Map);
@@ -100,12 +113,16 @@ private
       Queried_Node  : LAL.Ada_Node;
       Selector_Pattern : LEL.Selector_Pattern'Class)
       return Node_Iterator'Class;
+   --  Given a selector name N (ex: 'children'), return a Node iterator that
+   --  yields all the nodes bound to 'Queried_Node' by N.
 
    ------------------------
    -- Selector consumers --
    ------------------------
 
    package Selector_Consumers is new Node_Iterators.Consumers (Match);
+   --  Consummer for Iterators over 'Iterator_Node' values that produces
+   --  'Match' values as a result.
 
    subtype Node_Consumer is Selector_Consumers.Consumer_Interface;
 
@@ -113,6 +130,8 @@ private
    with record
       Pattern : LEL.Node_Pattern;
    end record;
+   --  Consumer that produces a successful 'Match' value if at least one of
+   --  the values yieded by the consumed iterator matches 'Pattern'.
 
    function Consume (Self : in out Exists_Consumer;
                      Iter : in out Node_Iterator'Class)
@@ -122,6 +141,9 @@ private
    with record
       Pattern : LEL.Node_Pattern;
    end record;
+   --  Consumer that produces a successful 'Match' value if all of the values
+   --  yielded by the consumed iterator match 'Pattern', or the iterator
+   --  doesn't yield any value.
 
    function Consume (Self : in out All_Consumer;
                      Iter : in out Node_Iterator'Class)
@@ -130,12 +152,15 @@ private
    function Make_Selector_Consumer (Selector     : LEL.Selector_Pattern;
                                     Related_Node : LEL.Node_Pattern)
                                     return Node_Consumer'Class;
+   --  Given a selector pattern, return a selector consumer that corresponds
+   --  to the quantifier specified in the pattern.
 
    type Selector_Conditions_Predicate is new Iterator_Predicate_Interface
    with record
       Context   : Eval_Context_Ptr;
       Condition : LEL.Expr;
    end record;
+   --  Predicate that evaluates a selector's filtering expression
 
    overriding function Evaluate
      (Self    : in out Selector_Conditions_Predicate;
