9df4c3e8189a84bafc09e544b9d643f177f3848f
Geoffrey Copin <copin@adacore.com>
2019-05-07 17:55:45 +0200

    Merge package 'Builtin_Functions' into 'Functions'
    Fun add

--- src/functions.adb
+++ src/functions.adb
@@ -1,8 +1,12 @@
-with Builtin_Functions;          use Builtin_Functions;
+with String_Utils;               use String_Utils;
 with Interpreter.Evaluation;     use Interpreter.Evaluation;
 with Interpreter.Error_Handling; use Interpreter.Error_Handling;
 
 with Langkit_Support.Text; use Langkit_Support.Text;
+
+with Ada.Strings.Wide_Wide_Unbounded; use Ada.Strings.Wide_Wide_Unbounded;
+with Ada.Strings.Wide_Wide_Unbounded.Wide_Wide_Text_IO;
+use Ada.Strings.Wide_Wide_Unbounded.Wide_Wide_Text_IO;
 
 package body Functions is
 
@@ -101,5 +105,57 @@ package body Functions is
       end loop;
 
    end Check_Arguments;
+
+   ---------------------
+   -- Is_Builtin_Call --
+   ---------------------
+
+   function Is_Builtin_Call (Call : L.Fun_Call) return Boolean is
+      (Call.F_Name.Text = "print" or else Call.F_Name.Text = "debug");
+
+   -----------------------
+   -- Eval_Builtin_Call --
+   -----------------------
+
+   function Eval_Builtin_Call
+     (Ctx : Eval_Context; Call : L.Fun_Call) return Primitive
+   is
+   begin
+      if Call.P_Arity /= 1 then
+         Raise_Invalid_Arity (Ctx, 1, Call.F_Arguments);
+      end if;
+
+      if Call.F_Name.Text = "print" then
+         return Eval_Print (Ctx, Call.F_Arguments.List_Child (1).P_Expr);
+      elsif Call.F_Name.Text = "debug" then
+         return Eval_Debug (Ctx, Call.F_Arguments.List_Child (1).P_Expr);
+      end if;
+
+      Raise_Unknown_Symbol (Ctx, Call.F_Name);
+   end Eval_Builtin_Call;
+
+   ----------------
+   -- Eval_Print --
+   ----------------
+
+   function Eval_Print (Ctx : Eval_Context; Expr : L.Expr) return Primitive is
+   begin
+      Display (Eval (Ctx, Expr));
+      return Make_Unit_Primitive;
+   end Eval_Print;
+
+   ----------------
+   -- Eval_Debug --
+   ----------------
+
+   function Eval_Debug (Ctx : Eval_Context; Node : L.Expr) return Primitive is
+      Code  : constant Text_Type := Node.Text;
+      Value : constant Primitive := Eval (Ctx, Node);
+      Message : constant Unbounded_Text_Type :=
+        Code & " = " & To_Unbounded_Text (Value);
+   begin
+      Put_Line (Message);
+      return Value;
+   end Eval_Debug;
 
 end Functions;
