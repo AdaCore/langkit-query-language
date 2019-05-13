27a6a829e46e31e067a3a772c7ef44271d9fa284
Geoffrey Copin <copin@adacore.com>
2019-05-13 11:07:12 +0200

    Add default parameter values for functions

--- src/functions.ads
+++ src/functions.ads
@@ -11,6 +11,9 @@ package Functions is
      (Ctx : Eval_Context; Call : L.Fun_Call) return Primitive;
    --  Evaluate a function call
 
+   function Is_Builtin_Call (Call : L.Fun_Call) return Boolean;
+   --  Return wether 'Call' is a call to a built-in function
+
 private
 
    function Eval_User_Fun_Call (Ctx  : Eval_Context;
@@ -19,8 +22,7 @@ private
    --  Evaluate a call to a user-defined function
 
    function Eval_Arguments (Ctx       : Eval_Context;
-                            Arguments : L.Arg_List;
-                            Def       : L.Fun_Def)
+                            Arguments : L.Named_Arg_Array)
                             return Environment_Map;
    --  Return a Map containing the argument's value along with their
    --  name.
@@ -34,9 +36,6 @@ private
    --     * There is no positonnal argument following a named argument
    --     * Named arguments are unique and match the name of a function
    --       parameter
-
-   function Is_Builtin_Call (Call : L.Fun_Call) return Boolean;
-   --  Return wether 'Call' is a call to a built-in function
 
    function Eval_Builtin_Call
      (Ctx : Eval_Context; Call : L.Fun_Call) return Primitive;
