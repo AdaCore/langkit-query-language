2ed7e85470d9b06d5f5334366dcaaac36279c1a0
Geoffrey Copin <copin.geoffrey@gmail.com>
2019-05-31 14:43:02 +0200

    Add preconditions in node matching functions

--- src/lkql-patterns-nodes.ads
+++ src/lkql-patterns-nodes.ads
@@ -11,13 +11,15 @@ package LKQL.Patterns.Nodes is
 
    function Match_Kind_pattern (Ctx     : Eval_Context;
                                 Pattern : L.Node_Kind_Pattern;
-                                Node    : LAL.Ada_Node) return Match_Result;
+                                Node    : LAL.Ada_Node) return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match th given node against a kind pattern
 
    function Match_Extended_Pattern (Ctx     : Eval_Context;
                                     Pattern : L.Extended_Node_Pattern;
                                     Node    : LAL.Ada_Node)
-                                    return Match_Result;
+                                    return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match the given node against an extended pattern
 
    function Matches_Kind_Name
@@ -28,7 +30,8 @@ package LKQL.Patterns.Nodes is
    function Match_Pattern_Details (Ctx     : Eval_Context;
                                    Details : L.Node_Pattern_Detail_List;
                                    Node    : LAL.Ada_Node)
-                                   return Match_Result;
+                                   return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match a given node agains the 'details' (fields, properties & sekectors)
    --  of a node pattern.
    --  The 'Bindings' part of the Match result will contain references to the
@@ -37,27 +40,31 @@ package LKQL.Patterns.Nodes is
    function Match_Pattern_Detail (Ctx    : Eval_Context;
                                   Node   : LAL.Ada_Node;
                                   Detail : L.Node_Pattern_Detail'Class)
-                                  return Match_Result;
+                                  return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match 'Node' against a node pattern 'detail'
 
    function Match_Pattern_Field (Ctx    : Eval_Context;
                                  Node   : LAL.Ada_Node;
                                  Field  : L.Node_Pattern_Field)
-                                 return Match_Result;
+                                 return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match the expected value specified in 'Field' against the value of the
    --  field of 'Node' designated by 'Field'.
 
    function Match_Pattern_Property (Ctx      : Eval_Context;
                                     Node     : LAL.Ada_Node;
                                     Property : L.Node_Pattern_Property)
-                                    return Match_Result;
+                                    return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match the exoected vaue specified in 'Property' agains the value of the
    --  property call described in 'Property' on 'Node'.
 
    function Match_Pattern_Selector (Ctx      : Eval_Context;
                                     Node     : LAL.Ada_Node;
                                     Selector : L.Node_Pattern_Selector)
-                                    return Match_Result;
+                                    return Match_Result
+     with Pre => not Node.Is_Null;
    --  Match 'Node' againt a selector apearing as a node pattern detail.
    --  If the selector has a binding name, a binding associating the said name
    --  to the output of the selector will be added to the 'Bindings' part of
