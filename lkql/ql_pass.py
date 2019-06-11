a252e6157da84baa8806896c4833e3f575a9f5ce
Geoffrey Copin <copin.geoffrey@gmail.com>
2019-06-11 11:55:36 +0200

    Add templatify / LKQL generation

--- lkql/ql_pass.py
+++ lkql/ql_pass.py
@@ -1,7 +1,30 @@
+from os import listdir, mkdir
+from os.path import join, isdir
+from shutil import rmtree
 from langkit.passes import GlobalPass
+from mako.template import Template
+
+TEMPLATES_DIR = 'lkql_src_templates'
+OUTPUT_DIR = 'lkql_generated_sources'
+
 
 def run(ctx):
-    print(ctx)
+    values = {
+        'lib_name': ctx.ada_api_settings.lib_name,
+        'root_name': ctx.astnode_types[0].entity.api_name
+    }
+
+    # if isdir(OUTPUT_DIR):
+    #     rmtree(OUTPUT_DIR)
+
+    if not isdir(OUTPUT_DIR):
+        mkdir(OUTPUT_DIR)
+
+    for src in listdir(TEMPLATES_DIR):
+        template = Template(filename=join(TEMPLATES_DIR, src))
+        with open(join(OUTPUT_DIR, src), 'w+') as f:
+            f.write(template.render(**values))
+
 
 def get_pass():
     return GlobalPass("generating the interpreter", run)
