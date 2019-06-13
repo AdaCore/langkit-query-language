42f15f5c860f01ce204ca70dcde51642d8880414
Geoffrey Copin <copin.geoffrey@gmail.com>
2019-06-13 12:34:36 +0200

    Add checks test to the testsuite

---
+++ testsuite/ada_projects/too_many_dependencies/pack1-flag.ads
@@ -0,0 +1,11 @@
+with Pack1;
+with Dep1;
+with Dep2;
+with Dep3;
+with Dep4;
+with Dep5;
+with Dep6;
+
+package Pack1.Flag is
+
+end Pack1.Flag;
---
+++ testsuite/ada_projects/too_many_dependencies/pack1-noflag.ads
@@ -0,0 +1,10 @@
+with Pack1;
+with Dep1;
+with Dep2;
+with Dep3;
+with Dep4;
+with Dep5;
+
+package Pack1.Noflag is
+
+end Pack1.Noflag;
