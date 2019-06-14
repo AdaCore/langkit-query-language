f796f17ccaef8c39b77ff67d101dc31ca764a4b4
Geoffrey Copin <copin.geoffrey@gmail.com>
2019-06-14 15:58:56 +0200

    Start implementing iterator adapters

---
+++ src/iters-adapters.adb
@@ -0,0 +1,54 @@
+package body Iters.Adapters is
+
+   ----------
+   -- Next --
+   ----------
+
+   function Next (Iter    : in out Filter_Adapter;
+                             Result  : out Wrapped_Iters.Element_Type)
+                             return Boolean
+   is
+      Element : Wrapped_Iters.Element_Type;
+   begin
+      if Iter.Wrapped.Next (Element)
+        and then Iter.Filter_Fn (Iter.Wrapped, Element)
+      then
+         Result := Element;
+         return True;
+      else
+         return False;
+      end if;
+   end Next;
+
+   -------------
+   -- Release --
+   -------------
+
+   overriding procedure Release (Iter : in out Filter_Adapter) is
+   begin
+      Wrapped_Iters.Release_Access (Iter.Wrapped);
+   end Release;
+
+   -----------
+   -- Clone --
+   -----------
+
+   overriding function Clone (Iter : Filter_Adapter) return Filter_Adapter is
+      Wrapped_Clone : constant Wrapped_Iters.Iterator_Access :=
+        new Wrapped_Iters.Iterator_Interface'Class'
+          (Wrapped_Iters.Iterator_Interface'Class (Iter.Wrapped.Clone));
+   begin
+      return Filter_Adapter'(Wrapped_Clone, Iter.Filter_Fn);
+   end Clone;
+
+   -------------------------
+   -- Make_Filter_Adapter --
+   -------------------------
+
+   function Make_Filter_Adapter (Iter : Wrapped_Iters.Iterator_Access;
+                                 Fn   : Filter_Func_Access)
+                                 return Filter_Adapter
+   is
+      (Filter_Adapter'(Iter, Fn));
+
+end Iters.Adapters;
