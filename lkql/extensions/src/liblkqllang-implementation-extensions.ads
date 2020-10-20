741ebad8c8280fc6ad9bcf3a2c6fb51377cff5ad
Raphaël AMIARD <raph.amiard@gmail.com>
2020-10-20 23:44:18 +0200

    SB05-065: Add forgotten lkql implementation files

---
+++ lkql/extensions/src/liblkqllang-implementation-extensions.ads
@@ -0,0 +1,6 @@
+package Liblkqllang.Implementation.Extensions is
+
+   function LKQL_Node_P_Prelude_Unit
+     (Node : Bare_LKQL_Node) return Internal_Unit;
+
+end Liblkqllang.Implementation.Extensions;
