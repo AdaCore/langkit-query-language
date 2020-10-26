b61ef010500085487e7cb8cc31ff62625c5e016c
Raphaël AMIARD <raph.amiard@gmail.com>
2020-10-26 19:09:27 +0100

    SB05-065: Add volatile objects test

---
+++ testsuite/tests/checks/volatile_objects/foo.ads
@@ -0,0 +1,16 @@
+with Interfaces, System, System.Storage_Elements;
+
+package Foo is
+   Variable: Interfaces.Unsigned_8
+      with Address => System.Storage_Elements.To_Address (0), Volatile;
+
+   Variable1: Interfaces.Unsigned_8                                --  FLAG
+      with Volatile;
+
+   type My_Int is range 1 .. 32 with Volatile;
+
+   Variable3 : My_Int;                                             --  FLAG
+
+   Variable4 : My_Int
+     with Address => Variable3'Address;
+end Foo;
