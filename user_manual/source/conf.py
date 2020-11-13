6dd42462fba53a445ba2ec4a13307d270e00e73f
Raphaël AMIARD <raph.amiard@gmail.com>
2020-11-13 10:51:31 +0100

    Add sphinx_rtd_theme option

--- user_manual/source/conf.py
+++ user_manual/source/conf.py
@@ -52,7 +52,13 @@ html_css_files = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if os.environ.get('USE_SPHINX_RTD_THEME'):
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = sphinx_rtd_theme.get_html_theme_path()
+else:
+    html_theme = 'alabaster'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
