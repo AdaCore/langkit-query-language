d84092fd4feca21bd2c0c0acbc0c1aa4426d3bd9
Raphaël AMIARD <raph.amiard@gmail.com>
2020-11-13 10:50:17 +0100

    Add railroad diagrams' css

--- user_manual/source/conf.py
+++ user_manual/source/conf.py
@@ -43,6 +43,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+html_css_files = [
+    'css/railroad.css'
+]
 
 # -- Options for HTML output -------------------------------------------------
 
