1dee9a8a712211ed8f1b4be713c2eda905cb270f
Raphaël AMIARD <raph.amiard@gmail.com>
2021-01-06 18:00:49 +0100

    [SB05-065] lkql_checker: Add Downward_View_Conversions rule

---
+++ testsuite/tests/checks/downward_view_conversions/test_downwardconv.adb
@@ -0,0 +1,33 @@
+procedure Main is
+   package Foo is
+      type T1 is tagged private;
+      procedure Proc1 (X : in out T1'Class);
+
+      type T2 is new T1 with private;
+      procedure Proc2 (X : in out T2'Class);
+
+   private
+      type T1 is tagged record
+         C : Integer := 0;
+      end record;
+
+      type T2 is new T1 with null record;
+   end Foo;
+
+   package body Foo is
+
+      procedure Proc1 (X : in out T1'Class) is
+         Var : T2 := T2 (X);                   --  FLAG
+      begin
+         Proc2 (T2'Class (X));                 --  FLAG
+      end Proc1;
+
+      procedure Proc2 (X : in out T2'Class) is
+      begin
+         X.C := X.C + 1;
+      end Proc2;
+
+   end Foo;
+begin
+   null;
+end Main;
