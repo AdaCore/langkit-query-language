4b7aab8474259a3c66cd33c61ddc7aa154ed11d9
Léo Germond <germond@adacore.com>
2021-02-09 12:40:30 +0100

    [U125-036] Function_Style_Procedures

---
+++ testsuite/ada_projects/checks/src/function_style_procedures.ads
@@ -0,0 +1,7 @@
+package Function_Style_Procedure is
+
+   procedure P (R : out Integer); -- FLAG
+   
+   procedure P2 (R : out Integer; R2: in out Integer);
+
+end Function_Style_Procedure;
--- testsuite/ada_projects/checks/src/main.adb
+++ testsuite/ada_projects/checks/src/main.adb
@@ -1,6 +1,7 @@
 with Abort_Statements;
 with Abstract_Type_Declarations;
 with Blocks;
+with Function_Style_Procedures;
 with Goto_Statements;
 
 procedure Main is
