437ac935d5361584bfc7c1dbce9dba7eeed2f5ed
Raphaël AMIARD <raph.amiard@gmail.com>
2021-02-10 12:43:01 +0100

    [SB05-065] lkql_checker: fix too_many_prims rule + add test

---
+++ testsuite/tests/checks/too_many_primitives/foo.ads
@@ -0,0 +1,10 @@
+package Foo is
+   type T is tagged null record;
+
+   procedure Foo (Self : T);
+   procedure Bar (Self : T);
+   procedure Baz (Self : T);
+   procedure Qux (Self : T);
+   procedure Ber (Self : T);
+   procedure Bleh (Self : T);
+end Foo;
