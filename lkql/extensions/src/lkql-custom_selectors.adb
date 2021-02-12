1d73a80ce1242bf51f1ec9ef1a5c5d7c1ab4baba
Raphaël AMIARD <raph.amiard@gmail.com>
2021-02-12 12:28:32 +0100

    [SB05-065] selectors: add an implicit "depth" parameter in selectors

--- lkql/extensions/src/lkql-custom_selectors.adb
+++ lkql/extensions/src/lkql-custom_selectors.adb
@@ -96,6 +96,7 @@ package body LKQL.Custom_Selectors is
    is
       Local_Ctx  : Eval_Context;
       Node_Value : constant Primitive := To_Primitive (Node.Node);
+      Dummy  : constant Primitive := To_Primitive (Node.Depth);
       Match_Data : constant Match_Array_Result :=
         Match_Pattern_Array (Iter.Ctx, Iter.Selector.P_Patterns, Node_Value);
    begin
@@ -105,6 +106,7 @@ package body LKQL.Custom_Selectors is
 
       Local_Ctx := Iter.Ctx.Create_New_Frame;
       Local_Ctx.Add_Binding ("it", Node_Value);
+      Local_Ctx.Add_Binding ("depth", To_Primitive (Node.Depth));
 
       for E of Iter.Selector.P_Nth_Expressions (Match_Data.Index) loop
          Add_Selector_Expr (Iter, Local_Ctx, Node.Depth, E.As_Selector_Expr);
@@ -128,6 +130,7 @@ package body LKQL.Custom_Selectors is
          then Eval (Local_Ctx, Expr.F_Expr.As_Unpack.F_Collection_Expr)
          else Eval (Local_Ctx, Expr.F_Expr));
    begin
+
       if Kind (Expr_Value) = Kind_Node then
          Add_Node (Iter, Depth, Node_Val (Expr_Value), Expr.F_Mode);
       elsif Kind (Expr_Value) in Sequence_Kind and then
@@ -158,6 +161,7 @@ package body LKQL.Custom_Selectors is
       With_Depth : constant Depth_Node :=
         Depth_Node'(Current_Depth + Depth_Offset, Node);
    begin
+
       if Node.Get.Is_Null_Node or else
         (Iter.Max_Depth >= 0 and then With_Depth.Depth > Iter.Max_Depth)
       then
