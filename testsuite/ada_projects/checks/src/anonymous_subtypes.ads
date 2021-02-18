47aa6121175b37fb0c058c86a3c77cdb69bd2136
Léo Germond <germond@adacore.com>
2021-02-18 16:46:38 +0100

    Anonymous_Subtypes

---
+++ testsuite/ada_projects/checks/src/anonymous_subtypes.ads
@@ -0,0 +1,14 @@
+package Anonymous_Subtypes is
+
+   type T is range 0 .. 10; -- NO FLAG
+
+   function F (I : Integer)
+               return Boolean is (I in 0 .. 10); -- FLAG
+   function F2 (I : Integer)
+                return Boolean is (case I is
+                                      when 0 .. 10 => True, -- FLAG
+                                      when others => False);
+   function F3 (I : Integer)
+                return Boolean is (for some J in 0 .. 10 => J > I); -- FLAG
+
+end Anonymous_Subtypes;
