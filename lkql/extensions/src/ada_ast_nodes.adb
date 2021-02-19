5dfcf6a29ce0cd7d491b5259245296c3dc3f8a0e
Pierre-Marie de Rodat <derodat@adacore.com>
2021-02-19 07:40:02 -0500

    Workaround GNAT bugs for coverage builds
    TN: TB30-009

--- lkql/extensions/src/ada_ast_nodes.adb
+++ lkql/extensions/src/ada_ast_nodes.adb
@@ -60,6 +60,32 @@ package body Ada_AST_Nodes is
                                   Target_Kind : Value_Kind)
                                   return I.Value_Type;
 
+   overriding function "=" (Left, Right : Ada_AST_Node) return Boolean is
+     (Left.Node = Right.Node);
+
+   overriding function Hash (Node : Ada_AST_Node) return Hash_Type is
+     (Hash (Node.Node));
+
+   overriding function Text_Image (Node : Ada_AST_Node) return Text_Type is
+      (To_Text (Node.Node.Image));
+
+   overriding function Kind_Name (Node : Ada_AST_Node) return String is
+     (Kind_Name (Node.Node));
+
+   overriding function Is_Null_Node (Node : Ada_AST_Node) return Boolean is
+     (Node.Node.Is_Null);
+
+   overriding function Children_Count (Node : Ada_AST_Node) return Natural is
+     (Node.Node.Children_Count);
+
+   overriding function Nth_Child
+     (Node : Ada_AST_Node; N : Positive) return Ada_AST_Node
+   is
+     (Ada_AST_Node'(Node => Node.Node.Child (N)));
+
+   function Make_Ada_AST_Node (Node : Ada_Node) return AST_Node_Rc
+   is (Make_AST_Node_Rc (Ada_AST_Node'(Node => Node)));
+
    --------------------------------------------------
    -- Node array to introspection value conversion --
    --------------------------------------------------
