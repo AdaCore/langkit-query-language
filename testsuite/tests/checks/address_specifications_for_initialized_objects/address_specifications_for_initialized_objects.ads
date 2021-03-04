bd858800633b61e675f1306da278b6fb24affa39
Léo Germond <germond@adacore.com>
2021-03-04 14:52:55 +0100

    Address_Specifications_For_Initialized_Objects
    Change-Id: I5b17601cbad958108758652b8f0de1d97bcb33a8

---
+++ testsuite/tests/checks/address_specifications_for_initialized_objects/address_specifications_for_initialized_objects.ads
@@ -0,0 +1,11 @@
+package Address_Specifications_For_Initialized_Objects is
+
+    I : Integer := 0;
+    Var0 : Integer with Address => I'Address;
+
+    Var1 : Integer := 10;
+    for Var1'Address use Var0'Address;                 --  FLAG
+
+    Var2 : Integer := 30 with Address => Var0'Address; --  FLAG
+
+end Address_Specifications_For_Initialized_Objects;
