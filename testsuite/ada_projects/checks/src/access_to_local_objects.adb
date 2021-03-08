74a5dad12adcec315e0dd46b1c8dd8d132006bfc
Léo Germond <germond@adacore.com>
2021-03-08 18:33:32 +0100

    Add protected body, entry, block

--- testsuite/ada_projects/checks/src/access_to_local_objects.adb
+++ testsuite/ada_projects/checks/src/access_to_local_objects.adb
@@ -8,16 +8,31 @@ package body Access_To_Local_Objects is
        Var2 :         Int_A := Var1'Access;  --  FLAG
        Var3 :         Int_A := Var0'Access;  --  NO FLAG
     begin
-        null;
+       null;
     end P;
 
     task T;
     task body T is
        type Int_A is access all Integer;
        Var4 : aliased Integer;
-       Var5 : Int_A := Var4'Access;  -- FLAG
+       Var5 : Int_A := Var4'Access;   -- FLAG
     begin
        null;
     end T;
+
+    protected Pro is
+       entry E;
+    end Pro;
+
+    protected body Pro is
+
+       entry E when True is
+           type Int_A is access all Integer;
+           Local : aliased Integer;
+           P : Int_A := Local'Access; -- FLAG
+       begin null;
+       end E;
+
+    end Pro;
 
 end Access_To_Local_Objects;
