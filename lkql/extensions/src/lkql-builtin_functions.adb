e3a2704564c886f44aa42f453798cd96e4d21a39
Raphaël AMIARD <raph.amiard@gmail.com>
2021-03-09 00:08:15 +0100

    [SB05-065] eval: Rework the built-in function mechanism
    Use regular scopes instead of a special case in the interpreter. Also
    make built-in functions first class values.

---
+++ lkql/extensions/src/lkql-builtin_functions.adb
@@ -0,0 +1,48 @@
+with LKQL.Evaluation; use LKQL.Evaluation;
+with Langkit_Support.Text; use Langkit_Support.Text;
+with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
+with Ada.Strings.Wide_Wide_Unbounded; use Ada.Strings.Wide_Wide_Unbounded;
+
+package body LKQL.Builtin_Functions is
+
+   ----------------
+   -- Eval_Print --
+   ----------------
+
+   function Eval_Print
+     (Ctx : access constant Eval_Context; Expr : L.Expr) return Primitive
+   is
+   begin
+      Display (Eval (Ctx.all, Expr));
+      return Make_Unit_Primitive;
+   end Eval_Print;
+
+   ----------------
+   -- Eval_Debug --
+   ----------------
+
+   function Eval_Debug
+     (Ctx : access constant Eval_Context; Node : L.Expr) return Primitive
+   is
+      Code    : constant Text_Type := Node.Text;
+      Value   : constant Primitive := Eval (Ctx.all, Node);
+      Message : constant Unbounded_Text_Type :=
+        Code & " = " & To_Unbounded_Text (Value);
+   begin
+      Put_Line (To_Text (Message));
+      return Value;
+   end Eval_Debug;
+
+   ------------------
+   -- Eval_To_List --
+   ------------------
+
+   function Eval_To_List
+     (Ctx : access constant Eval_Context; Node : L.Expr) return Primitive
+   is
+      Value : constant Primitive := Eval (Ctx.all, Node, Kind_Iterator);
+   begin
+      return To_List (Value.Get.Iter_Val.all);
+   end Eval_To_List;
+
+end LKQL.Builtin_Functions;
--- lkql/extensions/src/lkql-functions.adb
+++ lkql/extensions/src/lkql-functions.adb
@@ -1,10 +1,3 @@
-with Langkit_Support.Text; use Langkit_Support.Text;
-
-with Ada.Assertions;                  use Ada.Assertions;
-with Ada.Strings.Wide_Wide_Unbounded; use Ada.Strings.Wide_Wide_Unbounded;
-with Ada.Strings.Wide_Wide_Unbounded.Wide_Wide_Text_IO;
-use Ada.Strings.Wide_Wide_Unbounded.Wide_Wide_Text_IO;
-
 with LKQL.Selector_Lists;   use LKQL.Selector_Lists;
 with LKQL.AST_Nodes;        use LKQL.AST_Nodes;
 with LKQL.Depth_Nodes;      use LKQL.Depth_Nodes;
@@ -16,14 +9,17 @@ with LKQL.Node_Extensions; use LKQL.Node_Extensions;
 
 package body LKQL.Functions is
 
-   function Eval_Print (Ctx : Eval_Context; Expr : L.Expr) return Primitive;
-   --  Evaluate a call to the 'print' built-in function
+   function Eval_User_Fun_Call
+     (Ctx  : Eval_Context;
+      Call : L.Fun_Call;
+      Func : Primitive) return Primitive;
+   --  Evaluate a call to a user-defined function
 
-   function Eval_Debug (Ctx : Eval_Context; Node : L.Expr) return Primitive;
-   --  Evaluate a call to the 'debug' built-in function
-
-   function Eval_To_List (Ctx : Eval_Context; Node : L.Expr) return Primitive;
-   --  Evaluate a call to the 'debug' built-in function
+   function Eval_Builtin_Call
+     (Ctx  : Eval_Context;
+      Call : L.Fun_Call;
+      Fun  : Primitive) return Primitive;
+   --  Evaluate a call to a built-in function
 
    function Eval_User_Selector_Call
      (Ctx  : Eval_Context;
@@ -40,31 +36,29 @@ package body LKQL.Functions is
    is
       Func : Primitive;
    begin
-      --  If the call is a built-in function call, then eval this built-in
-      --  call.
-      if Call.P_Is_Builtin_Call then
-         return Eval_Builtin_Call (Ctx, Call);
-      end if;
-
       --  Else, eval the name to fetch the called entity
       Func := Eval (Ctx, Call.F_Name);
 
       --  Called entity should be a function or a selector
-      if Kind (Func) not in Kind_Function | Kind_Selector then
+      if Kind (Func) not in
+        Kind_Function | Kind_Selector | Kind_Builtin_Function
+      then
          Raise_Invalid_Type (Ctx, Call.As_LKQL_Node,
                              "function or selector", Func);
       end if;
 
       --  Call the proper eval sub function depending on the kind of the called
       --  entity.
-      if Kind (Func) = Kind_Function then
-         return Eval_User_Fun_Call (Ctx, Call, Func);
-      elsif Kind (Func) = Kind_Selector then
-         return Eval_User_Selector_Call (Ctx, Call, Func);
-      end if;
-
-      raise Program_Error with "unreachable";
-
+      case Kind (Func) is
+         when Kind_Function =>
+            return Eval_User_Fun_Call (Ctx, Call, Func);
+         when Kind_Selector =>
+            return Eval_User_Selector_Call (Ctx, Call, Func);
+         when Kind_Builtin_Function =>
+            return Eval_Builtin_Call (Ctx, Call, Func);
+         when others =>
+            raise Program_Error with "unreachable";
+      end case;
    end Eval_Call;
 
    ------------------------
@@ -240,57 +234,21 @@ package body LKQL.Functions is
    -----------------------
 
    function Eval_Builtin_Call
-     (Ctx : Eval_Context; Call : L.Fun_Call) return Primitive
+     (Ctx  : Eval_Context;
+      Call : L.Fun_Call;
+      Fun  : Primitive) return Primitive
    is
    begin
       if Call.P_Arity /= 1 then
          Raise_Invalid_Arity (Ctx, 1, Call.F_Arguments);
       end if;
 
-      if Call.F_Name.Text = "print" then
-         return Eval_Print (Ctx, Call.F_Arguments.List_Child (1).P_Expr);
-      elsif Call.F_Name.Text = "debug" then
-         return Eval_Debug (Ctx, Call.F_Arguments.List_Child (1).P_Expr);
-      elsif Call.F_Name.Text = "list" then
-         return Eval_To_List (Ctx, Call.F_Arguments.List_Child (1).P_Expr);
-      end if;
-
-      raise Assertion_Error with "should never happen";
+      declare
+         Builtin_Func : Builtin_Function_Access := Fun.Get.Fn_Access.all;
+      begin
+         return Builtin_Func
+           (Ctx'Access, Call.F_Arguments.List_Child (1).P_Expr);
+      end;
    end Eval_Builtin_Call;
-
-   ----------------
-   -- Eval_Print --
-   ----------------
-
-   function Eval_Print (Ctx : Eval_Context; Expr : L.Expr) return Primitive is
-   begin
-      Display (Eval (Ctx, Expr));
-      return Make_Unit_Primitive;
-   end Eval_Print;
-
-   ----------------
-   -- Eval_Debug --
-   ----------------
-
-   function Eval_Debug (Ctx : Eval_Context; Node : L.Expr) return Primitive is
-      Code  : constant Text_Type := Node.Text;
-      Value : constant Primitive := Eval (Ctx, Node);
-      Message : constant Unbounded_Text_Type :=
-        Code & " = " & To_Unbounded_Text (Value);
-   begin
-      Put_Line (Message);
-      return Value;
-   end Eval_Debug;
-
-   ------------------
-   -- Eval_To_List --
-   ------------------
-
-   function Eval_To_List (Ctx : Eval_Context; Node : L.Expr) return Primitive
-   is
-      Value : constant Primitive := Eval (Ctx, Node, Kind_Iterator);
-   begin
-      return To_List (Value.Get.Iter_Val.all);
-   end Eval_To_List;
 
 end LKQL.Functions;
