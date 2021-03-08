9592c13fdce9a589a190be02225825f9eb5eea75
Léo Germond <germond@adacore.com>
2021-03-08 16:26:10 +0100

    Add task as local scope

--- testsuite/ada_projects/checks/src/access_to_local_objects.adb
+++ testsuite/ada_projects/checks/src/access_to_local_objects.adb
@@ -2,13 +2,21 @@ package body Access_To_Local_Objects is
 
     Var0 : aliased Integer;
 
+    type Int_A is access all Integer;
     procedure P is
-       type Int_A is access all Integer;
        Var1 : aliased Integer;
        Var2 :         Int_A := Var1'Access;  --  FLAG
        Var3 :         Int_A := Var0'Access;  --  NO FLAG
     begin
         null;
     end P;
+
+    task T;
+    task body T is
+       Var4 : aliased Integer;
+       Var5 : Int_A := Var4'Access;  -- FLAG
+    begin
+       null;
+    end T;
 
 end Access_To_Local_Objects;
