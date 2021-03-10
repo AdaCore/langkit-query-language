a638b899de993f4be2585b9733909acc4462745a
Léo Germond <germond@adacore.com>
2021-03-10 14:45:11 +0100

    Fixup function_style_procedures

--- testsuite/ada_projects/checks/src/function_style_procedures.ads
+++ testsuite/ada_projects/checks/src/function_style_procedures.ads
@@ -1,7 +1,7 @@
-package Function_Style_Procedure is
+package Function_Style_Procedures is
 
    procedure P (R : out Integer); -- FLAG
    
    procedure P2 (R : out Integer; R2: in out Integer);
 
-end Function_Style_Procedure;
+end Function_Style_Procedures;
