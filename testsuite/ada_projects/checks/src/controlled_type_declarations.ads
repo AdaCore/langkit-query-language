e9403d264aa6838727e1ca4729d48ecff5312fc0
Léo Germond <germond@adacore.com>
2021-03-10 17:10:14 +0100

    fixup! Controlled_Type_Declarations init

--- testsuite/ada_projects/checks/src/controlled_type_declarations.ads
+++ testsuite/ada_projects/checks/src/controlled_type_declarations.ads
@@ -1,4 +1,6 @@
 with Ada.Finalization;
 package Foo is
    type Resource is new Ada.Finalization.Controlled with private;  --  FLAG
+private
+   type Resource is new Ada.Finalization.Controlled with null record;
 end Foo;
