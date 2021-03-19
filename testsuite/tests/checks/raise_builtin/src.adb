64578509be024a78c2e7b7e645543950453cb2e3
Arnaud Charlet <charlet@adacore.com>
2021-03-19 13:57:45 +0100

    Fix handling of exception renaming in raising_predefined_exceptions
    And adjust test accordingly
    
    Change-Id: I3562bc77a9cd9c456451261be7af7ea563b7281c

--- testsuite/tests/checks/raise_builtin/src.adb
+++ testsuite/tests/checks/raise_builtin/src.adb
@@ -1,7 +1,14 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Raise_Builtin is
+   Ren : exception renames Constraint_Error;
+   Ren2 : exception renames Ren;
 begin
-   raise Program_Error with "Message";
-   raise Funky_Error;
+   raise Program_Error with "Message";  --  FLAG
+   raise Funky_Error;                   --  NO FLAG
+   raise Ren;                           --  FLAG
+
+exception
+   when others =>
+      raise Ren2;                       --  FLAG
 end Raise_Builtin;
