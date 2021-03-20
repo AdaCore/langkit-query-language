62ef6256ce8bd9f2f8a3b42fde04d1aefec5ed0c
Arnaud Charlet <charlet@adacore.com>
2021-03-20 11:27:10 +0100

    exceptions_as_control_flow: fix handling of renamings
    Only one level of renaming was supported.
    Update test accordingly.
    
    Change-Id: I7ada53aae88bd0327843bb15863123f45d6957cb

--- testsuite/tests/checks/exceptions_as_control_flow/test2.adb
+++ testsuite/tests/checks/exceptions_as_control_flow/test2.adb
@@ -1,7 +1,15 @@
 procedure Test2 is
+   CE  : exception renames Constraint_Error;
+   CE2 : exception renames CE;
 begin
    begin
       raise Constraint_Error;  --  FLAG
+   exception
+      when CE => null;
+   end;
+
+   begin
+      raise CE2;  --  FLAG
    exception
       when Constraint_Error => null;
    end;
