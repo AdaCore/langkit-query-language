34a8bd6587814f48b4b22f096e8d8bdab88e0330
Arnaud Charlet <charlet@adacore.com>
2021-03-20 12:08:58 +0100

    Fix detection of expression functions in private parts
    Change-Id: Iab5e4d6dde9382b56adec68f099db8ff22d610fe

--- testsuite/tests/checks/expression_functions/foo.ads
+++ testsuite/tests/checks/expression_functions/foo.ads
@@ -1,4 +1,8 @@
 package Foo is
    function F (I : Integer) return Integer is   --  FLAG
      (if I > 0 then I - 1 else I + 1);
+
+private
+   function F2 return Boolean is  --  FLAG
+     (True);
 end Foo;
