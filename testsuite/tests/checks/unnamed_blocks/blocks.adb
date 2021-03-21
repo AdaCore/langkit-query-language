5f836043285a8e774a824a3f656f1720fa22921b
Arnaud Charlet <charlet@adacore.com>
2021-03-21 18:30:48 +0100

    Implement unnamed_blocks_and_loops rule
    Change-Id: I492d8fcd9eaee12430ce2ae40ce4d7883147d2c5

---
+++ testsuite/tests/checks/unnamed_blocks/blocks.adb
@@ -0,0 +1,35 @@
+procedure Blocks (S : in out String) is
+   I : Integer := 1;
+begin
+   if S'Length > 10 then
+      declare                                  --  FLAG
+         S1   : String (S'Range);
+         Last : Positive := S1'Last;
+         Idx  : Positive := 0;
+      begin
+         for J in S'Range loop                 --  FLAG
+            S1 (Last - Idx) := S (J);
+            Idx             := Idx + 1;
+
+            for K in S'Range loop              --  FLAG
+               S (K) := Character'Succ (S (K));
+            end loop;
+         end loop;
+         S := S1;
+      end;
+   end if;
+
+   begin          --  FLAG
+      null;
+   end;
+
+   Named: begin   --  NO FLAG
+      null;
+   end Named;
+
+   Named: loop    --  NO FLAG
+      loop        --  FLAG
+         null;
+      end loop;
+   end loop Named;
+end Blocks;
