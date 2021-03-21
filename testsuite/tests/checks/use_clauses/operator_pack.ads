622ecd6c4700caed2e5a2edc0c84c5f15a2da2a8
Arnaud Charlet <charlet@adacore.com>
2021-03-21 21:53:56 +0100

    Implement use_clauses rule
    Change-Id: Ie0bf64346cc842a17ec7722da42ea33a681c6c1f

---
+++ testsuite/tests/checks/use_clauses/operator_pack.ads
@@ -0,0 +1,3 @@
+package Operator_Pack is
+   function "+" (L, R : Character) return Character;
+end Operator_Pack;
---
+++ testsuite/tests/checks/use_clauses/pack2.ads
@@ -0,0 +1,6 @@
+with Pack, Operator_Pack;
+use Pack, Pack;             --  FLAG FLAG
+use Operator_Pack;          --  FLAG only if Exempt_Operator_Packages is not set
+
+package Pack2 is
+end Pack2;
