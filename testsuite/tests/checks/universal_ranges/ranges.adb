bce85df5581af5617d2f1f926e6f15cd1e0c5de7
Arnaud Charlet <charlet@adacore.com>
2021-03-21 15:51:10 +0100

    Implement universal_ranges rule
    Change-Id: Id3064215318a7066c23888dbbeee58597d2aa007

---
+++ testsuite/tests/checks/universal_ranges/ranges.adb
@@ -0,0 +1,16 @@
+procedure Ranges is
+
+   L : Positive := 1;
+   C : constant := 1;
+
+   S1 : String (L .. 10);     --  NO FLAG
+   S2 : String (1 .. 10);     --  FLAG
+   S3 : String (1 .. C);      --  FLAG
+
+   subtype S10 is String (1 .. 10);   --  FLAG
+
+begin
+   for J in 1 .. 10 loop   --  FLAG
+      null;
+   end loop;
+end Ranges;
