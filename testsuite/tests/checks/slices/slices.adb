bfa085a71453e8dc21ed52d22d70a12036e96a04
Arnaud Charlet <charlet@adacore.com>
2021-03-23 12:35:06 +0100

    Add detection of Array(Subtype) in slices rule
    Change-Id: I1809a11ae0890a2e87553cd19d89f67549d1f803

--- testsuite/tests/checks/slices/slices.adb
+++ testsuite/tests/checks/slices/slices.adb
@@ -3,6 +3,10 @@ procedure Slices (S : in out String; L, R : Positive) is
    T   : String (1 .. 10);            --  NO FLAG
    subtype S10 is String (1 .. 10);   --  NO FLAG
 
+   subtype Ind is Integer range 1 .. 5;
+
+   X : String := T (Ind);             --  FLAG
+
 begin
    null;
 end Slices;
