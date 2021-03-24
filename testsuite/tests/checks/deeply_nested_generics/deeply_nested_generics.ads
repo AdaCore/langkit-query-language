70c545f2115762eeaf86794ac90d08e4d7eb5753
Léo Germond <germond@adacore.com>
2021-03-24 16:56:13 +0100

    Deeply Nested Generics
    Change-Id: Ib4e8654fe8f1316735b9c5719b7b47db80b7b6e3

---
+++ testsuite/tests/checks/deeply_nested_generics/deeply_nested_generics.ads
@@ -0,0 +1,20 @@
+package Deeply_Nested_Generics is
+   generic
+   package P_G is
+   end;
+
+   generic
+   package P_G_0 is -- FLAG (depth=0)
+      generic procedure P;
+
+      generic
+        with package Formal is new P_G; -- NO_FLAG
+      package P_G_1 is --  FLAG (depth=1)
+         generic
+         package P_G_2 is -- FLAG (depth=2)
+            I  : Integer;
+         end;
+      end;
+   end;
+
+end Deeply_Nested_Generics;
