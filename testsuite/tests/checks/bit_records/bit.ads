47ebe8766a78946ab572b8b57666a2b7520a12e3
Arnaud Charlet <charlet@adacore.com>
2021-03-26 14:36:32 +0100

    Implement bit_records_without_layout_definition rule
    Change-Id: I11b16b1f18ff5e1879edd893427efe668c7eb0e7

---
+++ testsuite/tests/checks/bit_records/bit.ads
@@ -0,0 +1,20 @@
+package Bit is
+   type My_Mod is mod 8;
+
+   type Rec1 is record              --  NO FLAG
+      I : My_Mod;
+   end record;
+   for Rec1 use record
+      I at 8 range 0 ..63;
+   end record;
+
+   type Rec2_PP is record           --  FLAG
+      I : My_Mod;
+   end record;
+   for Rec2_PP'Size use 32;
+   pragma Pack (Rec2_PP);
+
+   type Rec2_PA is record           --  FLAG
+      I : My_Mod;
+   end record with Pack, Size => 32;
+end Bit;
