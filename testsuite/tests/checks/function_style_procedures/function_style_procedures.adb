17c77892b3c41c6b29836fe25d14f1f07076e8b6
Arnaud Charlet <charlet@adacore.com>
2021-03-27 19:35:44 +0100

    Complete implementation of function_style_procedures
    Change-Id: I8aa9411ca29b386bbbb9b76f1461bee014621cab

--- testsuite/tests/checks/function_style_procedures/function_style_procedures.adb
+++ testsuite/tests/checks/function_style_procedures/function_style_procedures.adb
@@ -1,16 +1,19 @@
 package body Function_Style_Procedures is
 
-
    procedure P (R : out Integer) is
    begin
       R := 0;
    end P;
-   
+
+   procedure P_Limited (R : out Lim) is
+   begin
+      null;
+   end P_Limited;
+
    procedure P2 (R : out Integer; R2: in out Integer) is
    begin
       R := R2;
       R2 := 0;
    end P2;
-   
 
 end Function_Style_Procedures;
--- testsuite/tests/checks/function_style_procedures/function_style_procedures.ads
+++ testsuite/tests/checks/function_style_procedures/function_style_procedures.ads
@@ -1,7 +1,12 @@
 package Function_Style_Procedures is
 
-   procedure P (R : out Integer); -- FLAG
+   procedure P (R : out Integer);  -- FLAG
+
+   type Lim is limited null record;
+   procedure P_Limited (R : out Lim);  -- NO FLAG
+
+   procedure P_Null (R : out Integer) is null;  -- NO FLAG
    
-   procedure P2 (R : out Integer; R2: in out Integer);
+   procedure P2 (R : out Integer; R2: in out Integer);  -- NO FLAG
 
 end Function_Style_Procedures;
