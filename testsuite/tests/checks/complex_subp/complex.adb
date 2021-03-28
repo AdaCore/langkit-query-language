6b30ed57fce6c2b85d7b0a9a9b41186ea96dbe9b
Arnaud Charlet <charlet@adacore.com>
2021-03-28 17:52:17 +0200

    Implement complex_inlined_subprograms rule
    Change-Id: Id7f8885313498adcd3c96e126ced4b063b78e2dd

---
+++ testsuite/tests/checks/complex_subp/complex.adb
@@ -0,0 +1,59 @@
+with Ada.Unchecked_Conversion;
+
+package body Complex is
+
+   procedure Swap (I, J : in out Integer) is   --  FLAG
+   begin
+      if I /= J then
+         declare
+            Tmp : Integer;
+         begin
+            TMP := I;
+            I   := J;
+            J   := Tmp;
+         end;
+      end if;
+   end Swap;
+
+   procedure Has_Decl with Inline is   --  FLAG
+      procedure Nested is
+      begin
+         null;
+      end Nested;
+   begin
+      null;
+   end Has_Decl;
+
+   procedure Many_Stmts;
+   pragma Inline (Many_Stmts);
+
+   procedure Many_Stmts is   --  FLAG
+      X : Integer;
+   begin
+      X := 1;
+      X := 1;
+      X := 1;
+      X := 1;
+      X := 1;
+      X := 1;
+   end Many_Stmts;
+
+   procedure With_UC with Inline is   --  NO FLAG
+      function Convert is new Ada.Unchecked_Conversion (Integer, Character);
+   begin
+      null;
+   end With_UC;
+
+   generic
+   procedure Gen_Proc;
+
+   procedure Gen_Proc is
+   begin
+      if True then
+         null;
+      end if;
+   end Gen_Proc;
+
+   procedure Inst_Proc is new Gen_Proc with Inline;   --  FLAG
+
+end Complex;
---
+++ testsuite/tests/checks/complex_subp/complex.ads
@@ -0,0 +1,6 @@
+package Complex is
+
+   procedure Swap (I, J : in out Integer);
+   pragma Inline (Swap);
+
+end Complex;
