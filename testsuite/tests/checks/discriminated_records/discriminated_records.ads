945dca4ba2d71ca521921342c8fa38dac58dcf27
Léo Germond <germond@adacore.com>
2021-03-29 09:57:55 +0200

    Discriminated Records
    Change-Id: I44b6b2378f80c1cd08f20507cd5d116753368322

---
+++ testsuite/tests/checks/discriminated_records/discriminated_records.ads
@@ -0,0 +1,26 @@
+package Discriminated_Records is
+    type Idx is range 1 .. 100;
+    type Arr is array (Idx range <>) of Integer;
+    subtype Arr_10 is Arr (1 .. 10);
+
+    type Rec_1 (D : Idx) is record        --  FLAG
+       A : Arr (1 .. D);
+    end record;
+
+    type Rec_2 (D : Idx) is record        --  FLAG
+       B : Boolean;
+    end record;
+
+    type Rec_3 is record
+       B : Boolean;
+    end record;
+
+    type Rec_2_Deriv (D : Idx) is new Rec_2(D); -- NO FLAG
+    type Rec_3_Deriv (D_Other : Idx) is new Rec_2(D => D_Other); -- FLAG
+
+    type Pv_Rec (D : Idx) is private; -- NO FLAG
+
+private
+    type Pv_Rec (D : Idx) is null record; -- FLAG
+
+end Discriminated_Records;
