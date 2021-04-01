25c938ccb4e68fd7a6d5b3a5ad9f18431ea5e936
Arnaud Charlet <charlet@adacore.com>
2021-04-01 20:21:37 +0200

    Implement positional_components rule
    Change-Id: I460ab0429b321d55122d2c6e446f449c325ab3e1

---
+++ testsuite/tests/checks/positional_components/pos.ads
@@ -0,0 +1,15 @@
+with Ada.Unchecked_Conversion;
+
+package Pos is
+   type Arr is array (1 .. 10) of Integer;
+
+   type Rec is record
+      C_Int  : Integer;
+      C_Bool : Boolean;
+      C_Char : Character;
+   end record;
+
+   Var_Rec_1 : Rec := (C_Int => 1, C_Bool => True, C_Char => 'a');  --  NO FLAG
+   Var_Rec_2 : Rec := (2, C_Bool => False, C_Char => 'b');   --  FLAG
+   Var_Rec_3 : Rec := (1, True, 'c');                        --  FLAG
+end Pos;
