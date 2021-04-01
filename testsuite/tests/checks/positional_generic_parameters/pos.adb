e05480f0f8319ea895068c557431f9b0c1f5c4f1
Arnaud Charlet <charlet@adacore.com>
2021-04-01 20:44:00 +0200

    Implement positional_generic_parameters rule
    Change-Id: Ib02d0f4d160d3101e2725e5a283b45b618b7d5c7

---
+++ testsuite/tests/checks/positional_generic_parameters/pos.adb
@@ -0,0 +1,15 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Unchecked_Conversion;
+procedure Pos (I : in out Integer) is
+   type My_Int is range -12345 .. 12345;
+
+   function To_My_Int is new Ada.Unchecked_Conversion
+     (Source => Integer, Target => My_Int);
+
+   function To_Integer is new Ada.Unchecked_Conversion
+     (My_Int, Integer);                                --  FLAG (twice)
+
+   package My_Int_IO is new Ada.Text_IO.Integer_IO (My_Int);  --  NO FLAG
+begin
+   null;
+end Pos;
