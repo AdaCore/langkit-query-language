f4a26c5281e8cbec6170b696e63b0df24dbbe84c
Arnaud Charlet <charlet@adacore.com>
2021-04-05 10:01:14 +0200

    Implement numeric_literals rule
    Change-Id: I9ac10c39027b16d9397293014a0788ecbe562196

---
+++ testsuite/tests/checks/numeric_literals/num.adb
@@ -0,0 +1,19 @@
+procedure Num is
+   CI1  : constant       := 1 + 2;      --  NO FLAG
+   CF1  : constant Float := 1.0 + 2.0;  --  NO FLAG
+   Ind1 : constant Positive := 1;       --  NO FLAG
+   Ind2 : constant Positive := 2;       --  NO FLAG
+   S1   : String (Ind1 .. Ind2);        --  NO FLAG
+   S2   : String (1 .. 2);              --  FLAG unless statements_only
+   I    : Integer := 3;                 --  FLAG unless statements_only
+   F    : Float   := CF1;               --  NO FLAG
+begin
+   S1 (1) := 'a';                       -- FLAG only with n < 1
+   S1 (Ind1) := 'a';                    -- NO FLAG
+   S1 (Ind1 + 2) := 'a';                -- FLAG only with n < 2
+
+   F := 1.0;                            -- FLAG
+   I := 2 + 0;                          -- FLAG only with n < 2
+   I := I + 2;                          -- FLAG only with n < 2
+   I := 3 * I;                          -- FLAG
+end Num;
