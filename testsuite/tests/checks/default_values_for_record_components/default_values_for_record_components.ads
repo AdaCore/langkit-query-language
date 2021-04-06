20c9d7e78c48d781b26ba9be1b3607cd930ada73
Léo Germond <germond@adacore.com>
2021-04-06 18:16:53 +0200

    Default Values For Record Components
    Change-Id: Id480a5d9da59384f9e0fe6cb734a78b2301203c6

---
+++ testsuite/tests/checks/default_values_for_record_components/default_values_for_record_components.ads
@@ -0,0 +1,13 @@
+package Default_Values_For_Record_Components is
+   type Rec (D : Natural := 0) is record
+      I : Integer := 0;                    -- FLAG
+      B : Boolean;
+   
+      case D is
+         when 0 =>
+            C : Character := 'A';          -- FLAG
+         when others =>
+            F : Float;
+      end case;
+   end record;
+end Default_Values_For_Record_Components;
