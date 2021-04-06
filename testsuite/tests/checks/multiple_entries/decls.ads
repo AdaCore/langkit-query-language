ceea931b31af3ad05dd1f18ceea9006291a6cf35
Arnaud Charlet <charlet@adacore.com>
2021-04-06 12:33:39 +0200

    Fix implementation of multiple_entries_in_protected_definitions
    Was not always returning a valid node and could be simplified.
    
    Change-Id: Ifcf5f98aed6dee76daf32f4ad3efbba8c9bd8dfc

--- testsuite/tests/checks/multiple_entries/decls.ads
+++ testsuite/tests/checks/multiple_entries/decls.ads
@@ -1,12 +1,17 @@
-package P is
+package Decls is
 
-protected P0 is
-    entry Get (I: Integer);
-    entry Put (I: out Integer);
-    procedure Reset;
-    function Check return Boolean;
-private
-    Val : Integer := 0;
-end P0;
+   protected P0 is
+      entry Get (I: Integer);       --  NO FLAG
+      entry Put (I: out Integer);   --  FLAG
+      entry Put2;                   --  FLAG
+      procedure Reset;
+      function Check return Boolean;
+   private
+      Val : Integer := 0;
+   end P0;
 
-end P;  
+   protected P1 is
+      entry E;  --  NO FLAG
+   end P1;
+
+end Decls;  
