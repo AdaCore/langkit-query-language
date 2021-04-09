00ac7833aeea00d08ebd88bfa8744dc013127856
Arnaud Charlet <charlet@adacore.com>
2021-04-09 17:10:33 +0200

    Complete address_attribute_for_non_volatile_objects
    Both implementation and test.
    Also fix stdlib.ultimate_alias to only use p_referenced_decl() on BaseId
    and DottedName.
    
    Change-Id: Id0f8f06cbf1d7e33789ca19c7067834b19244b37

--- testsuite/tests/checks/address_attribute_for_non_volatile_objects/address_attribute_for_non_volatile_objects.ads
+++ testsuite/tests/checks/address_attribute_for_non_volatile_objects/address_attribute_for_non_volatile_objects.ads
@@ -3,7 +3,31 @@ package Address_Attribute_For_Non_Volatile_Objects is
    Var1 : Integer with Volatile;
    Var2 : Integer;
 
-   X : Integer with Address => Var1'Address;
+   X : Integer with Address => Var1'Address;   --  NO FLAG
    Y : Integer with Address => Var2'Address;   --  FLAG
+
+   --  Address attribute specification clause:
+   Var_Non_Volatile : Integer;
+   for Var_Non_Volatile'Address use Var1'Address;     --  NO FLAG
+
+   --  Constant object:
+
+   Const : constant Integer := 123;
+   Var   : Integer with Address => Const'Address;     --  NO FLAG
+
+   --  Dereference:
+
+   type Int_Access is access all Integer;
+   Var_1 : aliased Integer;
+   Var_2 : Int_Access := Var_1'Access;
+   Var_3 : Integer with Address => Var_2.all'Address; --  NO FLAG
+
+   --  Renaming of array slice (not a data object):
+
+   type Arr is array (1 .. 10) of Integer;
+   Var_Arr   : Arr;
+   Var_Arr_R : Arr renames Var_Arr;
+   Var_Arr_5 : Integer renames Var_Arr_R (5);
+   V_Int_1 : Integer with Address => Var_Arr_5'Address;  --  NO FLAG
 
 end Address_Attribute_For_Non_Volatile_Objects;
