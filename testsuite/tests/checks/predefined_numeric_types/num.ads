7f57d89fd4c9be08aaf5ca9f37df7cb041f8640f
Arnaud Charlet <charlet@adacore.com>
2021-04-11 11:58:16 +0200

    Implement predefined_numeric_types rule
    Change-Id: Ieaafd9cf5b9f44960ce7e1921e9ec98056c8760b

---
+++ testsuite/tests/checks/predefined_numeric_types/num.ads
@@ -0,0 +1,14 @@
+package Num is
+
+   I : Integer;                               --  FLAG
+   F : Float;                                 --  FLAG
+   B : Boolean;                               --  NO FLAG
+
+   type Arr is array (1 .. 5) of Short_Float; --  FLAG
+
+   type Res is record
+      C1 : Long_Integer;                      --  FLAG
+      C2 : Character;                         --  NO FLAG
+   end record;
+
+end Num;
