503b306fbb9442ffd5ff614876e978bfcdb36d4d
Arnaud Charlet <charlet@adacore.com>
2021-04-11 09:16:53 +0200

    Implement specific_type_invariants rule
    Change-Id: Ie925df9560a20f42555ba38aea890fa9ce712824

---
+++ testsuite/tests/checks/specific_type_invariants/inv.ads
@@ -0,0 +1,19 @@
+package Inv is
+
+   type PT is private
+   with Type_Invariant => Test_PT (PT);            --  NO FLAG
+   function Test_PT (X : PT) return Boolean;
+
+   type TPT1 is tagged private
+   with Type_Invariant => Test_TPT1 (TPT1);        --  FLAG
+   function Test_TPT1 (X : TPT1) return Boolean;
+
+   type TPT2 is tagged private
+   with Type_Invariant'Class => Test_TPT2 (TPT2);  --  NO FLAG
+   function Test_TPT2 (X : TPT2) return Boolean;
+
+private
+   type PT is tagged null record;
+   type TPT1 is tagged null record;
+   type TPT2 is tagged null record;
+end Inv;
