7542e845ea3aceaca1063a649165230548866e9e
Arnaud Charlet <charlet@adacore.com>
2021-04-11 18:35:02 +0200

    Implement outer_loop_exits rule
    Change-Id: Ie1e13d6125539bc709dc27912778763dac059481

---
+++ testsuite/tests/checks/outer_loop_exits/loops.adb
@@ -0,0 +1,21 @@
+procedure Loops (S1, S2 : String) is
+   Detected : Boolean := False;
+begin
+   Outer : for J in S1'Range loop
+      for K in S2'Range loop
+         if S1 (J) = S2 (K) then
+            Detected := True;
+            exit Outer;                     --  FLAG
+         end if;
+      end loop;
+   end loop Outer;
+
+   for J in S1'Range loop
+      Outer2: for K in S2'Range loop
+         if S1 (J) = S2 (K) then
+            Detected := True;
+            exit Outer2;                    --  NO FLAG
+         end if;
+      end loop Outer2;
+   end loop;
+end Loops;
