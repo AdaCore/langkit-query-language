d10a4e5c64c7741ee01ab43f9fd270506b4615d5
Arnaud Charlet <charlet@adacore.com>
2021-04-14 10:12:24 +0200

    Implement number_declarations rule
    Change-Id: I2042675cd476bb68c92e37bcbf9aadad055bc009

---
+++ testsuite/tests/checks/number_decl/num.ads
@@ -0,0 +1,9 @@
+package Num is
+
+   Num1 : constant := 13;             --  FLAG
+   Num2 : constant := 1.3;            --  FLAG
+
+   Const1 : constant Integer := 13;   --  NO FLAG
+   Const2 : constant Float := 1.3;    --  NO FLAG
+
+end Num;
