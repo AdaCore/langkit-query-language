e4a5840d21b22e503e314610fac93709e31d7b99
Arnaud Charlet <charlet@adacore.com>
2021-04-16 15:38:10 +0200

    Implement non_short_circuit_operators rule
    Change-Id: I4a83892da2da42f2b14c99b430fb3e4ba81bbee8

---
+++ testsuite/tests/checks/non_short_circuit_operators/short_circuit.adb
@@ -0,0 +1,17 @@
+procedure Short_Circuit (I, J : Integer) is
+
+   type Mod_8 is mod 2**8;
+
+   function F1 (I, J : Mod_8) return Mod_8 is
+   begin
+      return "AND" (I, J);      --  NO FLAG
+   end F1;
+
+   B1 : Boolean;
+begin
+   B1 := I > 0 and J > 0;       --  FLAG
+   B1 := I < 0 and then J < 0;  --  NO FLAG
+   B1 := I > J or J > 0;        --  FLAG
+   B1 := I < J or else I < 0;   --  NO FLAG
+   B1 := "AND" (I < J, I < 0);  --  FLAG
+end Short_Circuit;
