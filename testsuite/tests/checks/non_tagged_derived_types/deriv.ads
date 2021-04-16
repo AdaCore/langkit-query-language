566ce0002383331cd8f0d83bfd9e05dd235d17fd
Arnaud Charlet <charlet@adacore.com>
2021-04-16 08:53:32 +0200

    Implement non_tagged_derived_types rule
    Change-Id: I3a7b14d124d1a7473633722cd4bcf4fdefc685ed

---
+++ testsuite/tests/checks/non_tagged_derived_types/deriv.ads
@@ -0,0 +1,39 @@
+package Deriv is
+
+   type Coordinates is record
+      X, Y, Z : Float;
+   end record;
+
+   type Hidden_Coordinates is new Coordinates;   --  FLAG
+
+   type T_Int is new Integer;                    --  FLAG
+
+   type T_Rec is tagged record
+      A : Integer;
+   end record;
+   type T_Rec_D is new T_Rec with record         --  NO FLAG
+      B : Boolean;
+   end record;
+
+   type T_Rec_2 is record
+      F : Integer;
+   end record;
+   type TD_Rec_2 is new T_Rec_2;                 --  FLAG
+
+   type T_Priv_1 is private;
+   type T_Priv_2 is abstract tagged private;
+
+   type T_Rec_DP is new T_Rec with private;      --  NO FLAG
+
+   procedure Proc (Param : T_Priv_1);
+
+private
+
+   type T_Priv_1 is new Positive range 1 .. 10;  --  FLAG
+   type T_Priv_2 is new T_Rec with record        --  NO FLAG
+      I : T_Priv_1;
+   end record;
+
+   type T_Rec_DP is new T_Rec with null record;  --  NO FLAG
+
+end Deriv;
