8e8a54965a2dc6a43a109e5112d30cc1b47785d5
Arnaud Charlet <charlet@adacore.com>
2021-04-17 13:15:25 +0200

    Implement no_inherited_classwide_pre rule
    Change-Id: I954b8de40e593324200d8e282c14102d7f278347

---
+++ testsuite/tests/checks/no_inherited_classwide_pre/class.adb
@@ -0,0 +1,20 @@
+package body Class is
+
+   function Test (X : NT1) return Boolean is
+   begin
+      return False;
+   end Test;
+
+   procedure Proc (X : in out NT1) is null;  --  NO FLAG
+   procedure Proc (X : in out NT2) is        --  NO FLAG
+   begin
+      null;
+   end Proc;
+
+   type NT3 is new T and Int1 with null record;
+   procedure Proc (X : in out NT3) is        --  FLAG
+   begin
+      null;
+   end Proc;
+
+end Class;
---
+++ testsuite/tests/checks/no_inherited_classwide_pre/class.ads
@@ -0,0 +1,27 @@
+package Class is
+
+   type Int is interface;
+   function Test (X : Int) return Boolean is abstract;
+   procedure Proc (I : in out Int) is abstract with Pre'Class => Test (I);
+
+   type Int1 is interface;
+   procedure Proc (I : in out Int1) is abstract;
+
+   type T is tagged private;
+
+   type NT1 is new T and Int with private;
+   function Test (X : NT1) return Boolean;        --  FLAG
+   procedure Proc (X : in out NT1);               --  NO FLAG
+
+   type NT2 is new T and Int1 with private;
+   procedure Proc (X : in out NT2);               --  FLAG
+
+private
+   type T is tagged record
+      I : Integer;
+   end record;
+
+   type NT1 is new T and Int with null record;
+   type NT2 is new T and Int1 with null record;
+
+end Class;
