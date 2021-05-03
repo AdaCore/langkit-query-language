6be4b4ba8be0f10d033f9fee1c388798bbea5a66
Arnaud Charlet <charlet@adacore.com>
2021-05-03 20:17:00 +0200

    Implement constructors rule
    Change-Id: I651d8d49f744e846db6609fbdec94187791ab72f

---
+++ testsuite/tests/checks/constructors/cons.ads
@@ -0,0 +1,41 @@
+package Cons is
+
+   type T is tagged record
+      I : Integer;
+   end record;
+
+   subtype ST_CW is T'Class;
+   type A_ST_CW is access ST_CW;
+
+   type A_T is access T;
+   subtype S_A_T is A_T;
+
+   function Fun (I : Integer) return T;                    -- FLAG
+   function Bar (JJJ : Integer) return T renames Fun;      -- FLAG
+   function Foo (KKK : Integer) return T is ((I => KKK));  -- FLAG
+
+   function CW (I : T'Class) return T;                     -- FLAG
+   function CW1 (I : ST_CW) return T;                      -- FLAG
+   function CW2 (I : A_ST_CW) return T;                    -- FLAG
+
+   function Fun1 (X : access T) return T;                  -- NO FLAG
+   function Fun2 (I : Integer) return access T;            -- FLAG
+   function Fun3 (I : S_A_T) return access T;              -- FLAG
+
+   type T1 is tagged private;
+
+   type T2 is tagged private;
+   function Fun_T2 return T2;                              -- FLAG
+
+   type T3 is tagged private;
+   function Fun_T3 (I : Integer) return T3;                -- FLAG
+
+   type T4 is tagged private;
+   function Fun_T4 (I : Integer) return T4;                -- FLAG
+
+private
+   type T1 is tagged null record;
+   type T2 is tagged null record;
+   type T3 is tagged null record;
+   type T4 is tagged null record;
+end Cons;
