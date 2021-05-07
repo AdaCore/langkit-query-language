c53a2efb4ffc780cb70c62bb8bcf91af4e3cc8c0
Arnaud Charlet <charlet@adacore.com>
2021-05-07 18:53:16 +0200

    Implement name_clashes rule
    Change-Id: I4a44f548d362cbb9456e8c52c0dafd59191fda58

---
+++ testsuite/tests/checks/name_clashes/name.ads
@@ -0,0 +1,7 @@
+package Name is
+
+   One          : constant Integer := 1;     --  FLAG
+   Two          : constant Float   := 2.0;   --  FLAG
+   Constant_One : constant Float   := 1.0;
+
+end Name;
