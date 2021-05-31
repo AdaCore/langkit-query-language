a43ade9d04ba33ea7688e566b51b785feef314b8
Raphaël AMIARD <raph.amiard@gmail.com>
2021-05-31 15:05:42 +0200

    [SB05-065] lkql: make objects' image json compliant
    Change-Id: Ife9ceec4ce6aa1e2dd61f7d7af8362ecb5178309

--- lkql/extensions/src/lkql-primitives.adb
+++ lkql/extensions/src/lkql-primitives.adb
@@ -202,7 +202,7 @@ package body LKQL.Primitives is
          I := 1;
 
          for Cur of Cursors loop
-            Append (Image, Key (Cur).all);
+            Append (Image, """" & Key (Cur).all & """");
             Append (Image, ": ");
             Append (Image, To_Unbounded_Text (Primitive_Maps.Element (Cur)));
             if I < Positive (Value.Elements.Length) then
