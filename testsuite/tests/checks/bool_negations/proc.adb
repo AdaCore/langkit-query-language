a634b2de0bf5eae4be369bfa335739c028dabb08
Arnaud Charlet <charlet@adacore.com>
2021-07-03 11:29:08 +0200

    Implement boolean_negations rule
    Change-Id: I17aaa3feff60788c3cc6b4b915cdebc055ecb96c
    TN: U629-018

---
+++ testsuite/tests/checks/bool_negations/proc.adb
@@ -0,0 +1,9 @@
+procedure Proc (Buffer_Length : Integer) is
+   Is_Data_Available : Boolean := Buffer_Length > 0;   --  NO FLAG
+begin
+   Is_Data_Available := not (Buffer_Length = 0);   --  FLAG
+
+   if not (not Is_Data_Available) then             --  FLAG
+      null;
+   end if;
+end Proc;
