baed6e6356ca15d3b9a299ffab880afe6ea2c390
Arnaud Charlet <charlet@adacore.com>
2021-07-04 21:57:38 +0200

    Add N parameter to default_parameters rule
    Change-Id: I5e91fce75a09c1db5f728a8701dc0356d37ba8d1
    TN: U629-018

--- testsuite/tests/checks/default_parameters/default_parameters.ads
+++ testsuite/tests/checks/default_parameters/default_parameters.ads
@@ -1,2 +1,5 @@
-procedure P (I : in out Integer; J : Integer := 0);   --  FLAG
-procedure Q (I : in out Integer; J : Integer);
+package Default_Parameters is
+   procedure P (I : in out Integer; J : Integer := 0);
+   procedure Q (I : in out Integer; J : Integer);
+   procedure R (I, J : Integer := 0; K : Integer := 0); --  FLAG
+end;
