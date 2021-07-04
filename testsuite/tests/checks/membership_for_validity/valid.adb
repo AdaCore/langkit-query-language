ca70df51ee7d0ff8b86dabad90223e3096c9df3b
Arnaud Charlet <charlet@adacore.com>
2021-07-04 20:47:52 +0200

    Implement membership_for_validity rule
    Change-Id: Ib8bd43833ff3f82c4280b03ea6bd7ba9815c03cf
    TN: U629-018

---
+++ testsuite/tests/checks/membership_for_validity/valid.adb
@@ -0,0 +1,19 @@
+procedure Valid is
+   subtype My_Int is Integer range 1 .. 10;
+
+   X : My_Int;
+   Y : Integer;
+
+begin
+   if X in My_Int then                           --  FLAG
+      null;
+   elsif X in My_Int'First .. My_Int'Last then   --  FLAG
+      null;
+   elsif X in Integer'First .. Integer'Last then --  NO FLAG
+      null;
+   elsif Y in Integer then                       --  FLAG
+      null;
+   elsif X in Integer then                       --  NO FLAG
+      null;
+   end if;
+end Valid;
