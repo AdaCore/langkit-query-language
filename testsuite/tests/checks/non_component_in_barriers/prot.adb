ef59f209d06b908e759199f2af9cf64ee52f5664
Arnaud Charlet <charlet@adacore.com>
2021-07-05 10:53:58 +0200

    Implement non_component_in_barriers rule
    Change-Id: I0f1f22b447e14ad25d1ce70c7aec9d07f63ceee0
    TN: U629-018

---
+++ testsuite/tests/checks/non_component_in_barriers/prot.adb
@@ -0,0 +1,37 @@
+procedure Prot is
+
+   protected Obj is
+      entry Get (V : out Integer);
+      entry Get2;
+      entry Get_KO;
+   private
+      Local  : Integer;
+      Is_Set : Boolean := False;
+   end Obj;
+
+   Global_Bool : Boolean := False;
+
+   protected body Obj is
+      entry Get (V : out Integer)
+        when Is_Set is             --  NO FLAG
+      begin
+         V := Local;
+         Is_Set := False;
+      end Get;
+
+      entry Get2
+        when True is               --  NO FLAG
+      begin
+         Is_Set := False;
+      end Get2;
+
+      entry Get_KO
+        when Global_Bool is        --  FLAG
+      begin
+         Global_Bool := False;
+      end Get_KO;
+   end Obj;
+
+begin
+   null;
+end Prot;
