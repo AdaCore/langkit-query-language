bfb7afbe19bee2d6d247c315293c7e899a6c8bc2
Arnaud Charlet <charlet@adacore.com>
2021-07-11 08:55:57 +0200

    Add support for Loops_Only param in overly_nested_control_structures
    Code cleanups.
    TN: U629-018
    TN: U706-013
    
    Change-Id: I01e5e45f40ee5815a2da35206649392fc7061eab

---
+++ testsuite/tests/checks/overly_nested_loop/nested.adb
@@ -0,0 +1,14 @@
+procedure Nested (I, J, Idx : in out Integer) is
+begin
+   loop
+      for Idx in I .. J loop
+         loop
+            if True then              --  NO FLAG
+               for K in 1 .. I loop   --  FLAG (if rule parameter is 2)
+                  null;
+               end loop;
+            end if;
+         end loop;
+      end loop;
+   end loop;
+end;
