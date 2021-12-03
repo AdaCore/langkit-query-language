47c0c8b22dae5d54a9cbe2631426900848e7c2e7
Arnaud Charlet <charlet@adacore.com>
2021-12-03 08:43:36 +0000

    unconstrained_array_returns: misc fixes
    Do not flag FormalSubpDecl.
    Flag SubpBodyStub.
    
    Change-Id: I2d5d4d8157fa298b8e357ec7fee96ad7ac5ca429

--- testsuite/tests/checks/unconstrained_array_returns/generics.ads
+++ testsuite/tests/checks/unconstrained_array_returns/generics.ads
@@ -7,7 +7,7 @@ package Generics is
 
    generic
       type Ret_Type (<>) is private;
-      with function Map_Value (X : Integer) return Ret_Type;   --  FLAG
+      with function Map_Value (X : Integer) return Ret_Type;   --  NO FLAG
    package Services is
      function Map (X : Integer) return Ret_Type;    --  FLAG
    end;
