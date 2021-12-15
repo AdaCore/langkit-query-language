48e7f2cfd5ae46167d84800d61094e8fcf4810eb
Arnaud Charlet <charlet@adacore.com>
2021-12-15 08:51:45 +0000

    integer_types_as_enum: also exclude derived types
    TN: UC14-031
    Change-Id: I7c52f85ce46081adf35b10947b92cedf59c5a072

--- testsuite/tests/checks/integer_as_enum/op.adb
+++ testsuite/tests/checks/integer_as_enum/op.adb
@@ -1,11 +1,16 @@
 procedure Op is
-   type Enum is range 1 .. 3;  --  FLAG
-   type Int is range 1 .. 3;   --  NO FLAG
+   type Enum is range 1 .. 3;    --  FLAG
+   type Int is range 1 .. 3;     --  NO FLAG
+
+   type Enum2 is range 1 .. 10;  --  NO FLAG
+   type Enum_D is new Enum2;
 
    X : Enum := 1;
    Y : Int := 1;
+   Z : Enum_D := 1;
 
 begin
    X := 2;
    Y := Y + 1;
+   Z := Z + 1;
 end Op;
