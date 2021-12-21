b6259d8eeb356a508d7bb058533f30089d06f8a8
Arnaud Charlet <charlet@adacore.com>
2021-12-21 19:21:36 +0000

    non_component_in_barriers: fix handling of components
    TN: UC21-020
    Change-Id: Ieaafca23d1570b88386acf0b7a28d8555237b074

---
+++ testsuite/tests/checks/non_component_in_barriers/comp.adb
@@ -0,0 +1,53 @@
+procedure Comp is
+
+   type Arr is array (1 .. 10) of Boolean;
+
+   type Rec is record
+      C : Boolean;
+   end record;
+
+   type Arr_Of_Rec is array (1 .. 10) of Rec;
+
+   protected Obj is
+      entry E1;
+      entry E2;
+      entry E3;
+      entry E4;
+   private
+      Is_Set : Boolean := False;
+
+      A   : Arr;
+      R   : Rec;
+      R1  : Rec;
+      A_R : Arr_Of_Rec;
+   end Obj;
+
+   Global_Bool : Boolean := False;
+
+   protected body Obj is
+
+      entry E1 when A (1) is          --  NO FLAG
+      begin
+         Is_Set := False;
+      end E1;
+
+      entry E2 when R.C is            --  NO FLAG
+      begin
+         Is_Set := True;
+      end E2;
+
+      entry E3 when A_R (1).C is      --  NO FLAG
+      begin
+         Is_Set := True;
+      end E3;
+
+      entry E4 when R = R1 is      --  NO FLAG
+      begin
+         Is_Set := True;
+      end E4;
+
+   end Obj;
+
+begin
+   null;
+end;
