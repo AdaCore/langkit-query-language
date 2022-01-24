35ca2ba3873d04eb7df4613c84c4ac0c9304c944
Arnaud Charlet <charlet@adacore.com>
2022-01-24 16:03:39 +0000

    unavailable_body_calls: fix support for indirect_calls
    And change the default, more compatible with gnatcheck
    rule argument processing (boolean parameters off by default,
    and enabled explicitly).
    
    Update test accordingly.
    
    TN: V111-038
    Change-Id: I066e1dee6416bd01b6a8e5e5b1ebe4b3082888fb

--- testsuite/tests/checks/unavailable_bodies/calls.adb
+++ testsuite/tests/checks/unavailable_bodies/calls.adb
@@ -5,6 +5,16 @@ procedure Calls is
    function X return Integer is (1);
 
    Val : Integer := X;      --  NO FLAG
+
+   type Proc_A is access procedure (X : Integer);
+   X : Proc_A;
+
+   type Proc_B is access procedure;
+   Y : Proc_B;
+
 begin
-   Unknown;                 --  FLAG
+   Unknown;     --  FLAG
+   X.all (1);   --  FLAG
+   X (1);       --  FLAG
+   Y.all;       --  FLAG
 end Calls;
