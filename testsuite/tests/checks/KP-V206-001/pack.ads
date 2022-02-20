c1e6065f977125ac58ab814c07d66c2ee7d9e221
Arnaud Charlet <charlet@adacore.com>
2022-02-20 12:44:11 +0000

    File KP detector for V206-001
    Change-Id: I73f370dc037c664513725ac9b31f72dac2294182

---
+++ testsuite/tests/checks/KP-V206-001/pack.ads
@@ -0,0 +1,14 @@
+with System.Address_To_Access_Conversions;
+
+package Pack is
+
+   type I1 is limited interface;
+   type Tag is tagged null record;
+
+   package Conversions is new System.Address_To_Access_Conversions  --  FLAG
+     (Object => I1'Class);
+
+   package Conv2 is new System.Address_To_Access_Conversions  --  NO FLAG
+     (Object => Tag);
+
+end Pack;
