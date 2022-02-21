f05a85f943a4ccf027bbf3ab511d9ffa8af15288
Arnaud Charlet <charlet@adacore.com>
2022-02-21 14:41:49 +0000

    Complete KP detector for UA12-036
    Add detector of non class wide UD instances even though these are much
    less likely, they correspond to the KP entry.
    
    Change-Id: I8acb2216919f987d64a9234fd0717281ecabc44c

--- testsuite/tests/checks/KP-UA12-036/p.ads
+++ testsuite/tests/checks/KP-UA12-036/p.ads
@@ -11,8 +11,12 @@ package P is
    type Nasty is new Root_Object with record
       X : Type1;
    end record;
+   type Nasty_Ptr is access all Nasty;
 
    procedure Free is  --  FLAG: Nasty is part of Root_Object'Class
      new Ada.Unchecked_Deallocation (Root_Object'Class, Root_Object_Ptr);
+
+   procedure Free is  --  FLAG
+     new Ada.Unchecked_Deallocation (Nasty, Nasty_Ptr);
 
 end P;
