c0b1addd88019f92497783e2324f8c02cff95888
Arnaud Charlet <charlet@adacore.com>
2022-02-21 11:32:37 +0000

    Make KP detector for UA12-036 more precise
    We now flag problematic instantiations of UD and point to the
    problematic type instead of flagging only the type.
    
    Change-Id: I27cc2e6829ea9e1fa6832cc0c25bb053403ede7a

--- testsuite/tests/checks/KP-UA12-036/p.ads
+++ testsuite/tests/checks/KP-UA12-036/p.ads
@@ -1,12 +1,18 @@
+with Ada.Unchecked_Deallocation;
+
 package P is
 
    type Root_Object is tagged null record;
+   type Root_Object_Ptr is access all Root_Object'Class;
 
    type Type1 is array (1..4) of Long_Float;
    for Type1'Alignment use 32;
 
-   type Nasty is new Root_Object with record  -- FLAG
+   type Nasty is new Root_Object with record
       X : Type1;
    end record;
+
+   procedure Free is  --  FLAG: Nasty is part of Root_Object'Class
+     new Ada.Unchecked_Deallocation (Root_Object'Class, Root_Object_Ptr);
 
 end P;
