ccda0cbadae7442d1f1bbd6a377036c5ffd54d87
Arnaud Charlet <charlet@adacore.com>
2022-04-15 15:09:18 +0000

    KP detector for S814-034
    Change-Id: I5bb9e9468270c7f6956d89ebf27d072ca898b422

---
+++ testsuite/tests/checks/KP-S814-034/p.adb
@@ -0,0 +1,17 @@
+procedure P is
+
+   Max_Error_Length : constant := 8;
+   subtype Str is String (1 .. Max_Error_Length);
+
+   type Rec is record
+      Text : Str;
+   end record;
+
+   type Arr is array (1 .. 16) of Rec;
+
+   Table : constant Arr :=
+     (3 => (Text => "INVALID "), others => (Text => "OTHERS  "));  --  FLAG
+   Table2 : constant Arr :=
+     (1 => (Text => "  VALID "), 2 => (Text => "OTHERS  "));       --  NO FLAG
+
+end;
