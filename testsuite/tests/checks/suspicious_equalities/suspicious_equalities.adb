2aa0cc97cdfb3e440329f21fb33eb46c657397f7
Arnaud Charlet <charlet@adacore.com>
2022-05-12 14:54:40 +0000

    suspicious_equalities: update test for consistency
    TN: V117-056
    Change-Id: Ie8c2ed3b08fdd8dbd0c4102afcd4a86779818b1c

--- testsuite/tests/checks/suspicious_equalities/suspicious_equalities.adb
+++ testsuite/tests/checks/suspicious_equalities/suspicious_equalities.adb
@@ -1,16 +1,16 @@
 procedure Suspicious_Equalities is
    X : Integer := 0;
 begin
-   if X = 1 and x = 2 then
+   if X = 1 and x = 2 then       --  FLAG
       null;
    end;
-   if X = 1 and then x = 2 then
+   if X = 1 and then x = 2 then  --  FLAG
       null;
    end;
-   if X /= 1 or x /= 2 then
+   if X /= 1 or x /= 2 then      --  FLAG
       null;
    end;
-   if X /= 1 or else x/= 2 then
+   if X /= 1 or else x/= 2 then  --  FLAG
       null;
    end;
 end;
