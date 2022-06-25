1a42e6ab7df8a6d029c1454e70eb67e35c98afa2
Arnaud Charlet <charlet@adacore.com>
2022-06-25 11:55:12 +0000

    KP detector for V616-018
    Change-Id: Ia64b974d9539a22502ea8bb8494ee63a29a69871

---
+++ testsuite/tests/checks/KP-V616-018/p.ads
@@ -0,0 +1,29 @@
+with Ada.Finalization; use Ada.Finalization;
+
+package P is
+   type Access_To_Array is access String;
+
+   type Parent is tagged record
+      F0 : Access_To_Array;  -- NO FLAG
+   end record;
+
+   type Child is new Parent with record
+      F1 : Integer;          -- NO FLAG
+      F2 : Access_To_Array;  -- FLAG
+      F3 : access String;    -- FLAG
+   end record;
+
+   generic
+      type T is private;
+      type T_Vector is array (Positive range <>) of T;
+
+   package Gen is
+
+      type T_Vector_Access is access all T_Vector;
+
+      type Vector is new Controlled with record
+         V : T_Vector_Access := null;   --  FLAG
+      end record;
+   end Gen;
+
+end;
