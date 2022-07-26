8d42750b1abc2e8554e1b763484abe7102ac14c3
Arnaud Charlet <charlet@adacore.com>
2022-07-26 10:07:17 +0000

    Add KP detector for V719-024
    Change-Id: I553480668026a933f06743bea91974740e5b6863

---
+++ testsuite/tests/checks/KP-V719-024/p.ads
@@ -0,0 +1,24 @@
+package P is
+
+  type Rec (D : Boolean := False) is record   --  FLAG
+    case D is
+      when False  => null;
+      when others => I : Integer;
+    end case;
+  end record with Dynamic_Predicate => True;
+
+  type Rec2 (D : Boolean := False) is record  --  NO FLAG
+    case D is
+      when False  => null;
+      when others => I : Integer;
+    end case;
+  end record;
+
+  type Rec3 (D : Boolean := False) is record   --  NO FLAG
+    case D is
+      when False => null;
+      when True  => I : Integer;
+    end case;
+  end record with Dynamic_Predicate => True;
+
+end P;
