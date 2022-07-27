d0156c82a8ad1184766e8d7af992831657ea7627
Arnaud Charlet <charlet@adacore.com>
2022-07-27 13:39:09 +0000

    Address TODO in metrics_cyclomatic_complexity
    And fix latent bug in metrics.eligible_children found when adding
    tests.
    
    Change-Id: Ib74419abcd21c3ecd49b9f5013dafa1c33065477

--- testsuite/tests/checks/metrics_cyclomatic/proc.adb
+++ testsuite/tests/checks/metrics_cyclomatic/proc.adb
@@ -1,9 +1,49 @@
---  if the rule parameter is 6 or less
-procedure Proc (I : in out Integer; S : String) is   --  FLAG
+procedure Proc (I : in out Integer; S : String) is   --  FLAG (7)
+   task type T;
+   protected type Prot is
+      procedure P1;
+      entry E1;
+   end Prot;
+
+   task body T is   --  FLAG (6)
+   begin
+      case I is
+         when 1 => null;
+         when 2 => null;
+         when 3 => null;
+         when 4 => null;
+         when 5 => null;
+         when others => null;
+      end case;
+   end T;
+
+   protected body Prot is
+      procedure P1 is  --  FLAG (6)
+      begin
+         case I is
+            when 1 => null;
+            when 2 => null;
+            when 3 => null;
+            when 4 => null;
+            when others => if I > 0 then null; end if;
+         end case;
+      end P1;
+
+      entry E1 when True is   --  FLAG (7)
+      begin
+         case I is
+            when 1 => null;
+            when 2 => null;
+            when 3 => null;
+            when 4 => null;
+            when others => I := if I > 0 then 1 elsif False then 2 else 3;
+         end case;
+      end E1;
+   end Prot;
+
 begin
    if I in 1 .. 10 then
       for J in S'Range loop
-
          if S (J) = ' ' then
             if I < 10 then
                I := 10;
