43cf1b72823774c5b2b78de52f73a6ef3fc50c27
Arnaud Charlet <charlet@adacore.com>
2022-09-17 12:06:05 +0000

    KP detector for V907-005
    Change-Id: Ie9f8de483938617cd5c6088772d36233685cd147

---
+++ testsuite/tests/checks/KP-V907-005/p.adb
@@ -0,0 +1,26 @@
+procedure P is
+   type Matrix is array (1 .. 10, 1 .. 10) of Integer;
+
+   A     : Matrix;
+   K     : Matrix;
+   Map   : array (0 .. 99) of Integer := (others => 0);
+   Key   : Integer := 0;
+   Value : Integer;
+   C     : constant Integer := 1;
+
+begin
+   for I in A'Range (1) loop        --  NO FLAG
+      for J in A'Range (2) loop     --  NO FLAG
+         A (I, J) := I * 10 + J;
+         K (I, J) := Key;
+         Map (C)  := Key;
+         key := Key + 1;
+      end loop;
+   end loop;
+
+   for I in A'Range (1) loop        --  NO FLAG
+      for J in A'Range (2) loop     --  FLAG
+         Map (K (I, J)) := I;
+      end loop;
+   end loop;
+end P;
