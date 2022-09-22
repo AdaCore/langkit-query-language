fb8d1b9181983f0115fd852c7c7df3feb595cb47
Arnaud Charlet <charlet@adacore.com>
2022-09-22 13:16:29 +0000

    KP detector for U310-012
    Change-Id: I2caf81873e9eb120b84bf7dc8bf46dd6e49b9a98

---
+++ testsuite/tests/checks/KP-U310-012/p.ads
@@ -0,0 +1,26 @@
+package P is
+
+   type T is record
+      D : Duration;
+   end record;
+
+   type T_Array is array (Positive range <>) of T;
+   type T_Array2 (<>) is private;
+   type Q (N : Natural) is private;
+
+   function Create return Q;                          --  FLAG
+   function Create return T_Array2;                   --  FLAG
+
+   function Create return T_Array with Post => True;  --  FLAG
+   function No_Flag return T_Array;                   --  NO FLAG
+
+private
+
+   type T_Array2 is array (Positive range <>) of T
+     with Type_Invariant => True;
+
+   type Q (N : Natural) is record
+      P : T_Array (1 .. N);
+   end record with Type_Invariant => True;
+
+end P;
