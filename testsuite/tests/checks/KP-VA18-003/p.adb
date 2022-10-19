27c1155f978504d40e3d892afd7bd15f69146116
Arnaud Charlet <charlet@adacore.com>
2022-10-19 10:36:38 +0000

    KP detector for VA18-003
    Change-Id: I035be92746ee9f5d22f23a9f92b43277b4a36865

---
+++ testsuite/tests/checks/KP-VA18-003/p.adb
@@ -0,0 +1,19 @@
+with Ada.Tags; use Ada.Tags;
+
+procedure P is
+   T : Tag := No_Tag;
+
+   type TT is tagged null record;
+begin
+   T := Descendant_Tag ("foo", T);   --  FLAG
+   T := Descendant_Tag ("foo", TT'Tag);   --  NO FLAG
+
+   if Is_Descendant_At_Same_Level (No_Tag, T) then   --  FLAG
+      declare
+         A : constant Tag_Array := Interface_Ancestor_Tags (No_Tag); --  FLAG
+         B : constant Tag_Array := Interface_Ancestor_Tags (TT'Tag); --  NO FLAG
+      begin
+         null;
+      end;
+   end if;
+end P;
