be0a428691fe873bdf320d227d1e72a37ae5bc6f
Arnaud Charlet <charlet@adacore.com>
2022-11-18 13:19:37 +0000

    KP detector for V708-029
    Change-Id: I04e31a91fa08260e2e3f156a2ae82b2de0565874

---
+++ testsuite/tests/checks/KP-V708-029/p.adb
@@ -0,0 +1,11 @@
+procedure P is
+   X : String (1 .. 3) := (Character'Val (96),
+                           Character'Val (220),
+                           Character'Val (255));
+
+   Int : Integer := 0;
+   S1  : String := X'Image;   --  FLAG
+begin
+   S1 := String'Image (X);   --  FLAG
+   S1 := Int'Image;          --  NO FLAG
+end P;
