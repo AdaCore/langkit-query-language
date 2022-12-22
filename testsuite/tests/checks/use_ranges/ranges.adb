3366b85856e7baacc881638f842017d94f5d6352
Arnaud Charlet <charlet@adacore.com>
2022-12-22 15:58:01 +0000

    use_ranges: new rule.
    TN: VC22-011
    Change-Id: Iff0c356aa43cd105f181c20db1ba0dfe4efbc979

---
+++ testsuite/tests/checks/use_ranges/ranges.adb
@@ -0,0 +1,27 @@
+procedure Ranges is
+   subtype My_Int is Integer range -20 .. -10;
+   type Str is new String (1 .. 10);
+   Obj : Str;
+   X   : Integer := 0;
+
+begin
+   for J in Integer'Range loop    --  FLAG
+      if J in Natural'Range then  --  FLAG
+         null;
+      elsif J in Natural'First .. Natural'Last then  --  FLAG
+         null;
+      elsif J in Obj'First .. Obj'Last then  --  FLAG
+         null;
+      end if;
+   end loop;
+
+   for J in Str'Range loop    --  NO FLAG
+      Obj (J) := ' ';
+   end loop;
+
+   case X is
+      when Obj'First .. Obj'Last => null;   --  FLAG
+      when My_Int'Range          => null;   --  FLAG
+      when others => null;
+   end case;
+end Ranges;
