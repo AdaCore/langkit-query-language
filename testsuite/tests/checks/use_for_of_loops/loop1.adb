0371725b6b17918bfa8980846cb874cb427ad84e
Arnaud Charlet <charlet@adacore.com>
2022-12-22 15:57:47 +0000

    use_for_of_loops: new rule.
    TN: VC22-011
    Change-Id: Ieb871cb28752159710a18ddc79278d4bb0e10e45

---
+++ testsuite/tests/checks/use_for_of_loops/loop1.adb
@@ -0,0 +1,18 @@
+procedure Loop1 (Str : in out String) is
+begin
+   for J in Str'Range loop    --  FLAG
+      if Str (J) = ' ' then
+         return;
+      elsif Str (J) = 'a' then
+         exit;
+      end if;
+   end loop;
+
+   for J in Str'Range loop    --  FLAG
+      Str (J) := ' ';
+   end loop;
+
+   for J in Str'Range loop    --  NO FLAG
+      exit when J > 1;
+   end loop;
+end Loop1;
