a998b40a2965ad9c9615399bc7e4862830a4996d
Arnaud Charlet <charlet@adacore.com>
2023-01-24 15:41:55 +0000

    Forbidden_Aspects: fix handling of AttributeRef
    Such as Pre'Class
    
    TN: VC22-011
    Change-Id: I638e9e0c2ef44a2f957bf586a4a972aa5d1fb0e3

--- testsuite/tests/checks/forbidden_aspects/aspects.ads
+++ testsuite/tests/checks/forbidden_aspects/aspects.ads
@@ -7,4 +7,12 @@ package Aspects is
      with Atomic,                 --  FLAG
      Volatile;                    --  NO FLAG
 
+   type T is tagged null record;
+
+   procedure Proc1 (X : in out T)
+     with Pre => True;            --  NO FLAG
+
+   procedure Proc2 (X : in out T)
+     with Pre'Class => True;      --  FLAG
+
 end Aspects;
