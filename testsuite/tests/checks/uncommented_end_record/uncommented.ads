b2fe4aba30671ab64b7c20e89eded634c6aa9bd4
Arnaud Charlet <charlet@adacore.com>
2023-01-27 08:59:02 +0000

    uncommented_end_record: skip extra whitespaces
    TN: W120-018
    Change-Id: I6a69693fd28aea6e8421e8b0907408de0ba648db

--- testsuite/tests/checks/uncommented_end_record/uncommented.ads
+++ testsuite/tests/checks/uncommented_end_record/uncommented.ads
@@ -1,15 +1,20 @@
 package Uncommented is
-   type R is record  --  NO FLAG
+   type R is record
       I : Integer;
-   end record;
+   end record;       --  NO FLAG
 
    type R is record
       I : Integer;
       J : Integer;
-   end record;     --  FLAG if N > 3
+   end record;     --  FLAG if N >= 3
 
-   type R2 is record  --  NO FLAG
+   type R2 is record
+      I1 : Integer;
+      I2 : Integer;
+   end record ;    -- R2  NO FLAG
+
+   type R2 is record
       I : Integer;
       J : Integer;
-   end record;     --  R2
+   end record;     --  R2  NO FLAG
 end;
