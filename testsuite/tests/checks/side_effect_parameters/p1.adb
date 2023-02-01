007772057d4b436eb01e4cddb1a6dfb95b8c9eaa
Arnaud Charlet <charlet@adacore.com>
2023-02-01 13:29:24 +0000

    side_effect_parameters: add support for function renamings
    TN: W120-018
    Change-Id: If3e98147382d731b3a64536f147e9f452e7eca28

--- testsuite/tests/checks/side_effect_parameters/p1.adb
+++ testsuite/tests/checks/side_effect_parameters/p1.adb
@@ -1,6 +1,7 @@
 package body P1 is
    function Read (A, B : Integer) return Integer with Import;
    function Read2 (A, B : Integer) return Integer with Import;
+   function Read_R return Integer renames Read;
 
    procedure P is
    begin
@@ -8,6 +9,7 @@ package body P1 is
       Proc (Read2, Read2);  --  FLAG
       Proc (Read, Read2);   --  NO FLAG
       Proc (Func1 (Read), Read);  --  FLAG
+      Proc (Read, Read_R);  --  FLAG
    end P;
 
 end P1;
