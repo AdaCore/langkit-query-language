cad87fa22a297a915d87c95dfa833f4cb99193eb
Arnaud Charlet <charlet@adacore.com>
2023-02-02 14:12:44 +0000

    side_effect_parameters: fix handling of calls with parameters
    We were counting calls with parameters twice: one on the Id of the
    subprogram, and one on the CallExpr.
    
    TN: W120-018
    Change-Id: Ib85542e17c8246786a8e0cb7ff5986b5b8b42515

--- testsuite/tests/checks/side_effect_parameters/p1.adb
+++ testsuite/tests/checks/side_effect_parameters/p1.adb
@@ -1,15 +1,16 @@
 package body P1 is
-   function Read (A, B : Integer) return Integer with Import;
-   function Read2 (A, B : Integer) return Integer with Import;
+   function Read (A : Integer) return Integer with Import;
    function Read_R return Integer renames Read;
 
    procedure P is
    begin
-      Proc (Read, Read);    --  FLAG
-      Proc (Read2, Read2);  --  FLAG
-      Proc (Read, Read2);   --  NO FLAG
-      Proc (Func1 (Read), Read);  --  FLAG
-      Proc (Read, Read_R);  --  FLAG
+      Proc (Read (1), Read (2)); --  FLAG
+      Proc (Read (1), 1);        --  NO FLAG
+      Proc (Read, Read);         --  FLAG
+      Proc (Read2, Read2);       --  FLAG
+      Proc (Read, Read2);        --  NO FLAG
+      Proc (Func1 (Read), Read); --  FLAG
+      Proc (Read, Read_R);       --  FLAG
    end P;
 
 end P1;
