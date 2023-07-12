b754f80b4b825da9257770f66183da95a3b270a3
Raphaël AMIARD <raph.amiard@gmail.com>
2023-07-12 17:18:22 +0200

    Add "ada_2022_in_ghost_code" rule
    Preliminary implementation for #73

---
+++ testsuite/tests/checks/ada_2022_in_ghost_code/test_flag_ada_2022.adb
@@ -0,0 +1,55 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Test is
+
+   type Point is record X, Y : Integer; end record;
+
+   type Int_Array is array (Positive range <>) of Integer;
+   Vals : Int_Array := (1, 2, 3, 4, 5, 6);
+   Sum : Integer := Vals'Reduce ("+", 0);  -- FLAG
+
+   Decl : Integer :=
+     (declare A : constant Integer := 12;
+              B : constant Integer := 15; begin A + B); -- FLAG
+
+   P : Point := (12, 15);
+   P2 : Point := (P with delta Y => 18);  -- FLAG
+
+   Vals_2 : Int_Array := (for I of Vals => I * 2); -- FLAG
+
+   protected A is
+   end A;
+
+   protected body A is
+      procedure Foo is null; --  FLAG
+      function Bar return Integer is (12); --  FLAG
+   end A;
+
+   type Str is null record with String_Literal => To_Str; --  FLAG
+
+   function To_Str (X : Wide_Wide_String) return Str is
+     (null record);
+
+   type Big_Int is null record with Integer_Literal => To_Big_Int; -- FLAG
+
+   function To_Big_Int (X : String) return Big_Int is (null record);
+
+
+   type Big_Real is null record with Real_Literal => To_Big_Real; -- FLAG
+
+   function To_Big_Real (X : String) return Big_Real is (null record);
+
+   function Foo (Bar: Integer with Test_Aspect) return Integer is (21);
+
+   generic
+      type T is private;
+      with function Foo (Self : T) return Integer
+        with Post => Foo'Result > 15; -- FLAG
+   package Bar is
+   end Bar;
+begin
+   Put_Line (Vals'Image);  --  FLAG
+   Put_Line (Sum'Image);  --  FLAG
+
+   Decl := @ + 12;  --  FLAG
+end Test;
