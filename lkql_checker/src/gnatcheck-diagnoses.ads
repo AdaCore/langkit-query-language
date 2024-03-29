aa97e70a4edc141453ecda30f619d65326e57faf
Hugo Guerrier <guerrier@adacore.com>
2024-03-29 12:47:22 +0100

    Update and harmonize copyright headers in GNATcheck sources

--- lkql_checker/src/gnatcheck-diagnoses.ads
+++ lkql_checker/src/gnatcheck-diagnoses.ads
@@ -1,27 +1,7 @@
-------------------------------------------------------------------------------
---                                                                          --
---                          GNATCHECK COMPONENTS                            --
---                                                                          --
---                  G N A T C H E C K . D I A G N O S E S                   --
---                                                                          --
---                                 S p e c                                  --
---                                                                          --
---                     Copyright (C) 2006-2023, AdaCore                     --
---                                                                          --
--- GNATCHECK  is  free  software;  you can redistribute it and/or modify it --
--- under terms of the  GNU  General Public License as published by the Free --
--- Software Foundation;  either version 2, or ( at your option)  any  later --
--- version.  GNATCHECK  is  distributed in the hope that it will be useful, --
--- but  WITHOUT  ANY  WARRANTY;   without  even  the  implied  warranty  of --
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
--- Public License for more details.  You should have received a copy of the --
--- GNU  General Public License distributed with GNAT; see file  COPYING. If --
--- not,  write to the  Free Software Foundation,  51 Franklin Street, Fifth --
--- Floor, Boston, MA 02110-1301, USA.                                       --
---                                                                          --
--- GNATCHECK is maintained by AdaCore (http://www.adacore.com).             --
---                                                                          --
-------------------------------------------------------------------------------
+--
+--  Copyright (C) 2005-2024, AdaCore
+--  SPDX-License-Identifier: GPL-3.0-or-later
+--
 
 --  This package defines routines for storing diagnostic messages and
 --  generating final gnatcheck report. It also provides routines that
