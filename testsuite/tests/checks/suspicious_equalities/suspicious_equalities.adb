00e0ec4b4f1cbfc4e557fce3f25d35494c0ab23a
Hugo Guerrier <hugogguerrier@gmail.com>
2024-06-07 11:54:20 +0200

    Add enumeration literals support in 'suspicious_equalities' rule

--- testsuite/tests/checks/suspicious_equalities/suspicious_equalities.adb
+++ testsuite/tests/checks/suspicious_equalities/suspicious_equalities.adb
@@ -1,16 +1,39 @@
 procedure Suspicious_Equalities is
+   type Enum is (E1, E2, E3);
+
+   B : Boolean := True;
+   E : Enum := E1;
    X : Integer := 0;
 begin
-   if X = 1 and x = 2 then       --  FLAG
+   if B = True and B = False then        --  FLAG
       null;
-   end;
-   if X = 1 and then x = 2 then  --  FLAG
+   end if;
+   if B = True and then B = False then   --  FLAG
       null;
-   end;
-   if X /= 1 or x /= 2 then      --  FLAG
+   end if;
+   if B /= True or B /= False then       --  FLAG
       null;
-   end;
-   if X /= 1 or else x/= 2 then  --  FLAG
+   end if;
+   if B /= True or else B /= False then  --  FLAG
       null;
-   end;
+   end if;
+   if E = E1 and then E = E2 then        --  FLAG
+      null;
+   end if;
+   if E /= E1 or else E /= E2 then         --  FLAG
+      null;
+   end if;
+
+   if X = 1 and x = 2 then               --  FLAG
+      null;
+   end if;
+   if X = 1 and then x = 2 then          --  FLAG
+      null;
+   end if;
+   if X /= 1 or x /= 2 then              --  FLAG
+      null;
+   end if;
+   if X /= 1 or else x/= 2 then          --  FLAG
+      null;
+   end if;
 end;
