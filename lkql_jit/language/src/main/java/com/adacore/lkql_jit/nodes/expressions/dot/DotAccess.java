bee1766ffaa12804607e2de46a80a0af1b14cb27
Hugo Guerrier <hugogguerrier@gmail.com>
2024-06-26 15:19:38 +0200

    Factorize all common features of 'DotAccess' and 'SafeDotAccess' in a base class

--- lkql_jit/language/src/main/java/com/adacore/lkql_jit/nodes/expressions/dot/DotAccess.java
+++ lkql_jit/language/src/main/java/com/adacore/lkql_jit/nodes/expressions/dot/DotAccess.java
@@ -6,42 +6,24 @@
 package com.adacore.lkql_jit.nodes.expressions.dot;
 
 import com.adacore.libadalang.Libadalang;
-import com.adacore.lkql_jit.LKQLContext;
-import com.adacore.lkql_jit.LKQLLanguage;
-import com.adacore.lkql_jit.built_ins.BuiltInMethodFactory;
-import com.adacore.lkql_jit.built_ins.BuiltInMethodValue;
 import com.adacore.lkql_jit.built_ins.values.*;
 import com.adacore.lkql_jit.exception.LKQLRuntimeException;
 import com.adacore.lkql_jit.nodes.Identifier;
-import com.adacore.lkql_jit.nodes.expressions.Expr;
 import com.adacore.lkql_jit.utils.Constants;
 import com.adacore.lkql_jit.utils.LKQLTypesHelper;
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
-import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.interop.ArityException;
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.UnsupportedMessageException;
-import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.object.DynamicObjectLibrary;
 import com.oracle.truffle.api.source.SourceSection;
-import java.util.Map;
 
 /**
  * This node represents the dot access in the LKQL language.
  *
  * @author Hugo GUERRIER
  */
-@NodeChild(value = "receiver", type = Expr.class)
-public abstract class DotAccess extends Expr {
-
-    // ----- Attributes -----
-
-    /** The member to access. */
-    protected final Identifier member;
+public abstract class DotAccess extends BaseDotAccess {
 
     // ----- Constructors -----
 
@@ -52,8 +34,7 @@ public abstract class DotAccess extends Expr {
      * @param member The member to access.
      */
     protected DotAccess(SourceSection location, Identifier member) {
-        super(location);
-        this.member = member;
+        super(location, member);
     }
 
     // ----- Execution methods -----
@@ -178,7 +159,6 @@ public abstract class DotAccess extends Expr {
      */
     @Fallback
     protected Object onOthers(Object receiver) {
-
         // In the fallback case, only built-in methods are candidates. Try to get a built-in.
         Object builtIn = this.tryBuiltIn(receiver);
 
@@ -188,67 +168,5 @@ public abstract class DotAccess extends Expr {
 
         throw LKQLRuntimeException.wrongMember(
                 this.member.getName(), LKQLTypesHelper.fromJava(receiver), this.member);
-    }
-
-    // ----- Class methods -----
-
-    /**
-     * Try to get a built-in method and execute it if there is no parameters.
-     *
-     * @param receiver The receiver object.
-     * @return The built-in result, null if the built-in method doesn't exist.
-     */
-    protected Object tryBuiltIn(Object receiver) {
-        // Get the built in
-        BuiltInMethodValue builtinMethod = this.getBuiltIn(receiver);
-        if (builtinMethod != null) {
-            if (builtinMethod.parameterNames.length <= 1) {
-                InteropLibrary builtInLibrary = InteropLibrary.getUncached(builtinMethod);
-                try {
-                    return builtInLibrary.execute(builtinMethod, builtinMethod.thisValue);
-                } catch (ArityException
-                        | UnsupportedTypeException
-                        | UnsupportedMessageException e) {
-                    throw LKQLRuntimeException.fromJavaException(e, this);
-                }
-            } else {
-                return builtinMethod;
-            }
-        }
-
-        // Return the null value if there is no method
-        return null;
-    }
-
-    /**
-     * Get the built-in method for the receiver.
-     *
-     * @param receiver The receiver object.
-     * @return The member if it exists, null else.
-     */
-    @CompilerDirectives.TruffleBoundary
-    protected BuiltInMethodValue getBuiltIn(Object receiver) {
-        // Get the LKQL context
-        LKQLContext context = LKQLLanguage.getContext(this);
-        Map<String, BuiltInMethodFactory> metaTable =
-                context.getMetaTable(LKQLTypesHelper.fromJava(receiver));
-
-        // Return the built-in method or null
-        if (metaTable.containsKey(this.member.getName())) {
-            return metaTable.get(this.member.getName()).instantiate(receiver);
-        } else {
-            return null;
-        }
-    }
-
-    // ----- Override methods -----
-
-    /**
-     * @see com.adacore.lkql_jit.nodes.LKQLNode#toString(int)
-     */
-    @Override
-    public String toString(int indentLevel) {
-        return this.nodeRepresentation(
-                indentLevel, new String[] {"member"}, new Object[] {this.member});
     }
 }
--- lkql_jit/language/src/main/java/com/adacore/lkql_jit/nodes/expressions/dot/SafeDotAccess.java
+++ lkql_jit/language/src/main/java/com/adacore/lkql_jit/nodes/expressions/dot/SafeDotAccess.java
@@ -6,40 +6,22 @@
 package com.adacore.lkql_jit.nodes.expressions.dot;
 
 import com.adacore.libadalang.Libadalang;
-import com.adacore.lkql_jit.LKQLContext;
-import com.adacore.lkql_jit.LKQLLanguage;
-import com.adacore.lkql_jit.built_ins.BuiltInMethodFactory;
-import com.adacore.lkql_jit.built_ins.BuiltInMethodValue;
 import com.adacore.lkql_jit.built_ins.values.LKQLNull;
 import com.adacore.lkql_jit.built_ins.values.LKQLProperty;
 import com.adacore.lkql_jit.exception.LKQLRuntimeException;
 import com.adacore.lkql_jit.nodes.Identifier;
-import com.adacore.lkql_jit.nodes.expressions.Expr;
 import com.adacore.lkql_jit.utils.LKQLTypesHelper;
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
-import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.interop.ArityException;
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.UnsupportedMessageException;
-import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.source.SourceSection;
-import java.util.Map;
 
 /**
  * This node represents the safe dot access in the LKQL language.
  *
  * @author Hugo GUERRIER
  */
-@NodeChild(value = "receiver", type = Expr.class)
-public abstract class SafeDotAccess extends Expr {
-
-    // ----- Attributes -----
-
-    /** The member to access. */
-    protected final Identifier member;
+public abstract class SafeDotAccess extends BaseDotAccess {
 
     // ----- Constructors -----
 
@@ -50,11 +32,8 @@ public abstract class SafeDotAccess extends Expr {
      * @param member The member to access.
      */
     protected SafeDotAccess(SourceSection location, Identifier member) {
-        super(location);
-        this.member = member;
+        super(location, member);
     }
-
-    public abstract Expr getReceiver();
 
     // ----- Execution methods -----
 
@@ -127,67 +106,5 @@ public abstract class SafeDotAccess extends Expr {
     protected void onGeneric(Object receiver) {
         throw LKQLRuntimeException.wrongType(
                 LKQLTypesHelper.ADA_NODE, LKQLTypesHelper.fromJava(receiver), this);
-    }
-
-    // ----- Class methods -----
-
-    /**
-     * Try to get a built-in method and execute it if there is no parameters.
-     *
-     * @param receiver The receiver object.
-     * @return The built-in result, null if the built-in method doesn't exist.
-     */
-    protected Object tryBuiltIn(Object receiver) {
-        // Get the built in
-        BuiltInMethodValue builtinMethod = this.getBuiltIn(receiver);
-        if (builtinMethod != null) {
-            if (builtinMethod.parameterNames.length <= 1) {
-                InteropLibrary builtInLibrary = InteropLibrary.getUncached(builtinMethod);
-                try {
-                    return builtInLibrary.execute(builtinMethod, builtinMethod.thisValue);
-                } catch (ArityException
-                        | UnsupportedTypeException
-                        | UnsupportedMessageException e) {
-                    throw LKQLRuntimeException.fromJavaException(e, this);
-                }
-            } else {
-                return builtinMethod;
-            }
-        }
-
-        // Return the null value if there is no method
-        return null;
-    }
-
-    /**
-     * Get the built-in method for the receiver.
-     *
-     * @param receiver The receiver object.
-     * @return The member if it exists, null else.
-     */
-    @CompilerDirectives.TruffleBoundary
-    protected BuiltInMethodValue getBuiltIn(Object receiver) {
-        // Get the LKQL context
-        LKQLContext context = LKQLLanguage.getContext(this);
-        Map<String, BuiltInMethodFactory> metaTable =
-                context.getMetaTable(LKQLTypesHelper.fromJava(receiver));
-
-        // Return the built-in method or null
-        if (metaTable.containsKey(this.member.getName())) {
-            return metaTable.get(this.member.getName()).instantiate(receiver);
-        } else {
-            return null;
-        }
-    }
-
-    // ----- Override methods -----
-
-    /**
-     * @see com.adacore.lkql_jit.nodes.LKQLNode#toString(int)
-     */
-    @Override
-    public String toString(int indentLevel) {
-        return this.nodeRepresentation(
-                indentLevel, new String[] {"member"}, new Object[] {this.member});
     }
 }
