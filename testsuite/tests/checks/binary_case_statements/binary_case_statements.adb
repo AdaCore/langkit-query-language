9f8d6085981eeb61ec9f9a690934a7767ab5d6a5
Hugo Guerrier <guerrier@adacore.com>
2024-07-08 10:29:28 +0200

    Auto-fix: Add support for 'binary_case_statements'
    (cherry picked from commit 33e34375a5a21df3d1be0e968cf1076cfed010eb)

--- testsuite/tests/checks/binary_case_statements/binary_case_statements.adb
+++ testsuite/tests/checks/binary_case_statements/binary_case_statements.adb
@@ -1,11 +1,29 @@
-package body Binary_Case_Statements is
-    procedure body P is
-    begin
-        case 1 is                   --  FLAG
-           when 1 =>
-              null;
-           when others =>
-              null;
-        end case;
-    end P;
-end Binary_Case_Statements;
+procedure Main is
+   procedure Test (I : Integer);
+begin
+   case 1 is  --  FLAG
+      when 1 => null;
+      when others => null;
+   end case;
+
+   case 1 is  --  FLAG
+      when 1 => Test (2);
+      when 2 => Test (1);
+   end case;
+
+   case True is  --  FLAG
+      when True => null;
+      when False => null;
+   end case;
+
+   case 1 is  --  NOFLAG
+      when 1 | 2 => null;
+      when others => null;
+   end case;
+
+   case 1 is  --  NOFLAG
+      when 1 => null;
+      when 2 => null;
+      when others => null;
+   end case;
+end Main;
