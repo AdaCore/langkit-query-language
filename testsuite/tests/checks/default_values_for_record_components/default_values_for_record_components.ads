979e617f436c6c002c6a3c6f6a4cdb474385f730
Hugo Guerrier <guerrier@adacore.com>
2024-07-08 11:17:12 +0200

    Auto-fix: Add support for 'default_values_for_record_components'

--- testsuite/tests/checks/default_values_for_record_components/default_values_for_record_components.ads
+++ testsuite/tests/checks/default_values_for_record_components/default_values_for_record_components.ads
@@ -1,13 +1,13 @@
 package Default_Values_For_Record_Components is
-   type Rec (D : Natural := 0) is record
+   type Rec (D : Natural := 0) is record   --  NOFLAG
       I : Integer := 0;                    --  FLAG
       B : Boolean;
-   
+
       case D is
          when 0 =>
             C : Character := 'A';          --  FLAG
          when others =>
-            F : Float;
+            F : Float;                     --  NOFLAG
       end case;
    end record;
 end Default_Values_For_Record_Components;
