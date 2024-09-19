3e7c98c5a92e3922a005a5b2ea22e25d704c806d
Hugo Guerrier <hugogguerrier@gmail.com>
2024-09-19 15:32:11 +0200

    Forward LKQL engine options through a JSON encoded string
    Remove all ad-hoc encoding methods and now forward all LKQL options as a JSON
    object which can be serialized into a Java class.
    
    (cherry picked from commit fcc49d689fdddbf81f0ab67cd60d793d9c847b3c)

--- lkql_jit/options/src/main/java/com/adacore/lkql_jit/JSONUtils.java
+++ lkql_jit/options/src/main/java/com/adacore/lkql_jit/JSONUtils.java
@@ -7,34 +7,14 @@ package com.adacore.lkql_jit; //
 
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 /** This class contains all util functions to handle JSON */
 public final class JSONUtils {
-
-    /** Serialize the given rule instance map to a JSON string. */
-    public static String serializeInstances(final Map<String, RuleInstance> instances) {
-        final JSONObject res = new JSONObject();
-        for (var instance : instances.entrySet()) {
-            res.put(instance.getKey(), instance.getValue().toJson());
-        }
-        return res.toString();
-    }
-
-    /**
-     * Deserialize the given JSON source to a rule instance map.
-     *
-     * @throws JSONException If the given source cannot be deserialized.
-     */
-    public static Map<String, RuleInstance> deserializeInstances(final String jsonSources)
-            throws JSONException {
-        JSONObject jsonInstances = new JSONObject(new JSONTokener(jsonSources));
-        return jsonInstances.keySet().stream()
-                .collect(
-                        Collectors.toMap(
-                                s -> s,
-                                s -> RuleInstance.fromJson(jsonInstances.getJSONObject(s))));
+    /** Given a JSON object, parse it as a String to String map and return the result. */
+    public static Map<String, String> parseStringMap(JSONObject jsonObject) {
+        return jsonObject.toMap().entrySet().stream()
+                .map(e -> Map.entry(e.getKey(), (String) e.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
