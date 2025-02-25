ef7b6ba8bebe16895dce2e35bebcf37e3cd1c3a3
Laurent Thévenoux <thevenoux@adacore.com>
2025-02-25 14:24:27 +0100

    Fix positional_generic_parameter rule
    The rule was failing in case a generic instantiation with only one
    parameter refered to a generic renaming declaration.
    
    This change also fixes how generic formal parameters are counted.

--- testsuite/tests/checks/positional_generic_parameters/pos.adb
+++ testsuite/tests/checks/positional_generic_parameters/pos.adb
@@ -10,6 +10,38 @@ procedure Pos (I : in out Integer) is
      (My_Int, Integer);                                --  FLAG (2)
 
    package My_Int_IO is new Ada.Text_IO.Integer_IO (My_Int);  --  NOFLAG
+
+   generic
+      A, B, C : Integer;
+   package G is
+   end G;
+
+   package I_G_1 is new G (1, 2, 3);  -- FLAG (3)
+   package I_G_2 is new G (1, 2, C => 3);  -- FLAG (2)
+   package I_G_3 is new G (1, C =>  2, B => 3);  -- FLAG (1)
+
+   generic package R_G renames G;
+
+   package I_RG is new R_G (1, 2, 3); -- FLAG (3)
+
+   generic
+      A : Integer;
+   package H is
+   end H;
+
+   package I_H is new H (1);  -- NOFLAG
+
+   generic package R_H renames H;
+
+   package I_RH is new R_H (1); -- NOFLAG
+
+   generic
+      A, B, C : Integer := 0;
+   package D is
+   end D;
+
+   package I_D is new D (1);  -- FLAG
+
 begin
    null;
 end Pos;
