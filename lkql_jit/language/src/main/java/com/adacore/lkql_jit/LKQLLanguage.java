f9ef8adf497eb8eef7114e29bc2ab72f5e6ca246
Hugo Guerrier <guerrier@adacore.com>
2025-04-09 13:12:50 +0200

    Add doc about the reason of 'tabStop = 1' in LKQL analysis context creation
    (cherry picked from commit 7a5ad1f137b462dd34b9d59625080024c958f97a)

--- lkql_jit/language/src/main/java/com/adacore/lkql_jit/LKQLLanguage.java
+++ lkql_jit/language/src/main/java/com/adacore/lkql_jit/LKQLLanguage.java
@@ -100,6 +100,9 @@ public final class LKQLLanguage extends TruffleLanguage<LKQLContext> {
     /** A simple constructor for the library loading. */
     public LKQLLanguage() {
         super();
+        // We create an LKQL analysis context with a tab-stop size of 1 since Truffle.Source
+        // columns counting is based on characters:
+        // https://www.graalvm.org/truffle/javadoc/com/oracle/truffle/api/source/Source.html#createSection(int,int,int,int)
         this.lkqlAnalysisContext =
                 Liblkqllang.AnalysisContext.create(null, null, null, null, true, 1);
 
