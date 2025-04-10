81e3618131cd3c107607fd42e5f8435e08ffa782
Laurent Thévenoux <thevenoux@adacore.com>
2025-04-10 14:12:31 +0200

    checker: fix downward view conversions false positive
    (cherry picked from commit f70142fa07bc4f3c712215a8a182caa1ab4943d5)

--- testsuite/tests/checks/downward_view_conversions/test_downwardconv.adb
+++ testsuite/tests/checks/downward_view_conversions/test_downwardconv.adb
@@ -36,6 +36,33 @@ procedure Main is
       end Proc2;
 
    end Foo;
+
+   procedure No_Flag is
+      package P is
+         type T is tagged private;
+         subtype S is T;
+         type T_Access is access all T;
+         type T_Constant_Access is access constant T;
+
+         type U is new T with private;
+      private
+         type T is tagged null record;
+         type U is new T with null record;
+      end P;
+
+      package body P is
+         function F (X : T_Access) return T_Constant_Access
+         is (T_Constant_Access (X)); -- NOFLAG
+
+         function G (X : U) return T is (T (X)); -- NOFLAG
+
+         function I (X : T) return T is (T (X)); -- NOFLAG
+
+         function J (X : T) return S is (S (X)); -- NOFLAG
+      end P;
+   begin
+      null;
+   end No_Flag;
 begin
    null;
 end Main;
