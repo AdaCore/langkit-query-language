7e5ae45961d094b444247b72c81aff9a7d1fe9d7
Laurent Thévenoux <thevenoux@adacore.com>
2025-04-30 16:40:52 +0200

    Fix explicit_full_discrete_ranges rule
    Do not consider range specification from type declarations.
    
    (cherry picked from commit 3c52aa6e2f2495b6cf119a24f869a34c8bac8e46)

--- testsuite/tests/checks/explicit_full_discrete_ranges/explicit_full_discrete_ranges.ads
+++ testsuite/tests/checks/explicit_full_discrete_ranges/explicit_full_discrete_ranges.ads
@@ -1,3 +1,8 @@
 package Explicit_Full_Discrete_Ranges is
+   type T1 is new Float range 1.0 .. 2.0;
+
+   type T2 is delta 0.1 range T1'First .. T1'Last;           -- NOFLAG
+   type T3 is delta 0.1 digits 7 range T1'First .. T1'Last;  -- NOFLAG
+
    procedure P;
 end Explicit_Full_Discrete_Ranges;
