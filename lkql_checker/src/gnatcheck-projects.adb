3a583908461e29c165e104bfdcc105e64ea04c21
Hugo Guerrier <guerrier@adacore.com>
2025-05-12 15:36:13 +0200

    Rename 'Rule_File_Name' global variable to 'Legacy_Rule_File_Name'

--- lkql_checker/src/gnatcheck-projects.adb
+++ lkql_checker/src/gnatcheck-projects.adb
@@ -1228,11 +1228,11 @@ package body Gnatcheck.Projects is
                        (File,
                         To_Unbounded_String (Parameter (Parser => Parser))));
                   if not More_Then_One_Rule_File_Set then
-                     Rule_File_Name :=
+                     Legacy_Rule_File_Name :=
                        new String'(Parameter (Parser => Parser));
                      More_Then_One_Rule_File_Set := True;
                   else
-                     Free (Rule_File_Name);
+                     Free (Legacy_Rule_File_Name);
                   end if;
 
                when others =>
