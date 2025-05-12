55134eba4994792a39bc91952ef9108dcf451a6c
Hugo Guerrier <guerrier@adacore.com>
2025-05-12 13:18:14 +0200

    Rename 'Add_Rule_Option' function to 'Add_Legacy_Rule_Option'

--- lkql_checker/src/gnatcheck-projects.adb
+++ lkql_checker/src/gnatcheck-projects.adb
@@ -1091,11 +1091,11 @@ package body Gnatcheck.Projects is
       Process_Compiler_Instances;
    end Process_Rule_Options;
 
-   ---------------------
-   -- Add_Rule_Option --
-   ---------------------
+   ----------------------------
+   -- Add_Legacy_Rule_Option --
+   ----------------------------
 
-   procedure Add_Rule_Option (Opt : String; Prepend : Boolean := False) is
+   procedure Add_Legacy_Rule_Option (Opt : String; Prepend : Boolean := False) is
       use Ada.Strings.Unbounded;
 
       Opt_Rec : constant Option_Record :=
@@ -1106,7 +1106,7 @@ package body Gnatcheck.Projects is
       else
          Rule_Options.Append (Opt_Rec);
       end if;
-   end Add_Rule_Option;
+   end Add_Legacy_Rule_Option;
 
    ----------------------
    -- Add_Rule_By_Name --
@@ -1118,7 +1118,7 @@ package body Gnatcheck.Projects is
       Prefix     : constant String :=
         (if Lower_Rule = "all" then "+" else "+R");
    begin
-      Add_Rule_Option (Prefix & Lower_Rule, Prepend => Prepend);
+      Add_Legacy_Rule_Option (Prefix & Lower_Rule, Prepend => Prepend);
    end Add_Rule_By_Name;
 
    ------------------------
@@ -1236,7 +1236,7 @@ package body Gnatcheck.Projects is
                   end if;
 
                when others =>
-                  Add_Rule_Option (Full_Switch (Parser => Parser));
+                  Add_Legacy_Rule_Option (Full_Switch (Parser => Parser));
                   Individual_Rules_Set := True;
             end case;
             if not Rules_Depreciation_Emitted then
