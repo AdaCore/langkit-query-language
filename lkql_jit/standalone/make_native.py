2b45fa8d88bb1a49fe64b668e81c1029c95109c0
Hugo Guerrier <guerrier@adacore.com>
2025-05-16 09:54:01 +0200

    Rework the 'native' module into a 'standalone' module
    This new module will contains all dependencies to run LKQL in both
    native-image and JVM modes.
    Update the native-image building script to be compatible with the new
    GraalVM version.
    Taking example from what is done for Simplelanguage.

--- lkql_jit/standalone/make_native.py
+++ lkql_jit/standalone/make_native.py
@@ -46,39 +46,88 @@ import subprocess
 import sys
 
 sys.path.append("..")
-from utils import GraalManager, parse_args, missing_module
+from utils import GraalManager, parse_args, is_windows
+
+def look_for_files_in_env(files: list[str], env_var_name: str) -> dict[str, str] | None:
+    """
+    Look for required `files` in directories listed in the value of the
+    environment variable `env_var_name`. Return the dictionary associating each
+    input file to the directory containing it. If one of the requested file
+    cannot be retrieved this function returns `None` and displays error message
+    about file not being found.
+    """
+    res = {f: None for f in files}
+    for dir in os.environ.get(env_var_name, "").split(os.pathsep):
+        for file in files:
+            if os.path.isfile(os.path.join(dir, file)):
+                res[file] = dir
+                break
+    one_not_found = False
+    for file, dir in res.items():
+        if dir is None:
+            one_not_found = True
+            print(f"Cannot find \"{file}\" in {env_var_name}")
+    return None if one_not_found else res
 
 if __name__ == "__main__":
     # Create utils
     graal = GraalManager()
     args = parse_args()
 
-    # Get the language and CLI JARs and verify their existences
-    language_jar = P.join("..", "language", "target", "lkql_jit.jar")
-    if not P.isfile(language_jar):
-        raise missing_module("language", language_jar)
+    # Prepare system dependant native-image options
+    os_specific_options = []
+    if not is_windows():
+        # On Linux, we need to provide the compiler additional arguments for
+        # it to find headers and shared objects.
+        headers_paths = look_for_files_in_env(
+            ["libadalang.h", "liblkqllang.h"],
+            "C_INCLUDE_PATH",
+        )
+        libs_paths = look_for_files_in_env(
+            ["libadalang.so", "liblkqllang.so", "libz.so"],
+            "LIBRARY_PATH",
+        )
+        if headers_paths is None or libs_paths is None:
+            print("Missing lib dependencies, cannot continue")
+            exit(1)
 
-    cli_jar = P.join("..", "cli", "target", "lkql_cli.jar")
-    if not P.isfile(cli_jar):
-        raise missing_module("cli", cli_jar)
+        # We also need to provide rpath-links to the compiler to allow it to
+        # find libraries during linking phase.
+        ld_library_path = os.environ.get('LD_LIBRARY_PATH')
+        rpaths = (
+            [f"-Wl,-rpath-link={p}" for p in ld_library_path.split(os.pathsep)]
+            if ld_library_path else
+            []
+        )
+
+        os_specific_options.extend([
+            # The G1 garbage collector is only supported on Linux for now
+            "--gc=G1",
+
+            # Then we add additional options for the C compiler
+            *[f"--native-compiler-options=-I{dir}" for dir in headers_paths.values()],
+            *[f"--native-compiler-options=-L{dir}" for dir in libs_paths.values()],
+            *[f"--native-compiler-options={rp}" for rp in rpaths],
+        ])
 
     # Run the Native-Image compiler if required by the build process
     if "lkql_cli" in args.native_components:
         # Create the base Native-Image command
         cmd = [
             graal.native_image,
-            "--macro:truffle",
+            "-cp", args.classpath,
             "--no-fallback",
-            "--language:regex",
-            "--initialize-at-build-time=com.adacore.lkql_jit,com.adacore.liblkqllang",
-            "-H:-UseContainerSupport",
+            "--initialize-at-build-time",
+            *os_specific_options,
         ]
 
+        # Add debug and development options
         if args.build_mode in ("dev", "debug"):
             cmd.extend(
                 [
                     "-g",
-                    "-O0",
+                    "-Ob",
+                    "-H:+UnlockExperimentalVMOptions",
                     "-H:-DeleteLocalSymbols",
                     "-H:+SourceLevelDebug",
                     "-H:+PreserveFramePointer",
@@ -86,12 +135,16 @@ if __name__ == "__main__":
                 ]
             )
             if args.build_mode == "debug":
-                cmd.append("-H:+PrintRuntimeCompileMethods")
+                cmd.extend(
+                    [
+                        "-H:+PrintRuntimeCompileMethods",
+                        "-H:+TraceNativeToolUsage",
+                    ]
+                )
 
+        # Finally specify the main class name and the output file
         final_cmd = cmd + [
-            "-cp",
-            os.pathsep.join([language_jar, cli_jar]),
-            f"com.adacore.lkql_jit.cli.LKQLMain",
+            "com.adacore.lkql_jit.cli.LKQLMain",
             P.join("target", "lkql"),
         ]
 
--- lkql_jit/utils.py
+++ lkql_jit/utils.py
@@ -58,28 +58,7 @@ class GraalManager:
                 if is_windows()
                 else P.join(self.home, "bin", "native-image")
             )
-            if not P.isfile(self._native_image):
-                raise RuntimeError(
-                    "Native-image tool is not available in your GraalVM"
-                    "installation, install it with 'gu'"
-                )
         return self._native_image
-
-    @property
-    def jar(self):
-        return (
-            P.join(self.home, "bin", "jar.exe")
-            if is_windows()
-            else P.join(self.home, "bin", "jar")
-        )
-
-    @property
-    def gu(self):
-        return (
-            P.join(self.home, "bin", "gu.cmd")
-            if is_windows()
-            else P.join(self.home, "bin", "gu")
-        )
 
 
 def is_windows():
@@ -106,17 +85,7 @@ def parse_args():
         help="Define the JIT build mode",
     )
     parser.add_argument("--native-components", help="Native components to build")
-    parser.add_argument("--lkql-version", help="The version of LKQL you're building")
-    parser.add_argument(
-        "--graal-version", help="Version of GraalVM you're building with"
-    )
+    parser.add_argument("--classpath", help="Classpath to forward")
 
     # Parse the command line and return the result
     return parser.parse_args()
-
-
-def missing_module(name, file):
-    return RuntimeError(
-        f"File '{P.realpath(file)}' not found. Make sure you ran `mvn package`"
-        f" for the '{name}' module."
-    )
