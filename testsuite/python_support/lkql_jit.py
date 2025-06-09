9a87d423c2d39451f677c8e438f7ddc8645c8075
Raphaël AMIARD <amiard@adacore.com>
2025-06-09 14:57:25 +0200

    Debug: Add performance instrumentation option to lkql_jit.py

--- testsuite/python_support/lkql_jit.py
+++ testsuite/python_support/lkql_jit.py
@@ -6,38 +6,81 @@ Python wrapper to call the Java version of LKQL JIT.
 import os
 import os.path as P
 import subprocess
-import sys
+import argparse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        "--truffle-debug", action="store_true",
+        help="Activate many options for truffle that allows the user to debug"
+        "the JIT"
+    )
+    parser.add_argument(
+        "--compile-immediately", action="store_true",
+        help="Activate an option for truffle that triggers automatic"
+        "compilation of root nodes. Useful to debug performance issues"
+    )
+    args, remaining = parser.parse_known_args()
 
     # Get the utils paths
-    graal_home = os.environ['GRAAL_HOME']
+    graal_home = os.environ["GRAAL_HOME"]
     lkql_jit_home = os.environ.get(
-        'LKQL_JIT_HOME',
-        P.join(graal_home, 'languages', 'lkql')
+        "LKQL_JIT_HOME", P.join(graal_home, "languages", "lkql")
     )
 
     # Get the Java executable
-    java = P.join(graal_home, 'bin', 'java.exe' if os.name == 'nt' else 'java')
+    java = P.join(graal_home, "bin", "java.exe" if os.name == "nt" else "java")
 
     # Create the class path
-    class_path = os.pathsep.join([
-        P.join(graal_home, 'lib', 'truffle', 'truffle-api.jar'),
-        P.join(lkql_jit_home, 'lkql_jit.jar'),
-        P.join(lkql_jit_home, 'lkql_cli.jar')
-    ])
+    class_path = os.pathsep.join(
+        [
+            P.join(graal_home, "lib", "truffle", "truffle-api.jar"),
+            P.join(lkql_jit_home, "lkql_jit.jar"),
+            P.join(lkql_jit_home, "lkql_cli.jar"),
+        ]
+    )
 
     # Create the java.library.path property
     java_library_path = os.environ.get(
-        'PATH' if os.name == 'nt' else "LD_LIBRARY_PATH", ""
+        "PATH" if os.name == "nt" else "LD_LIBRARY_PATH", ""
     )
 
     # Run the full command
-    subprocess.run([
-        java,
-        '-cp', class_path,
-        f'-Djava.library.path={java_library_path}',
-        f'-Dtruffle.class.path.append={P.join(lkql_jit_home, "lkql_jit.jar")}',
-        '--add-opens=org.graalvm.truffle/com.oracle.truffle.api.strings=ALL-UNNAMED',
-        'com.adacore.lkql_jit.cli.LKQLMain'
-    ] + sys.argv[1:])
+    subprocess.run(
+        [java, "-cp", class_path, f"-Djava.library.path={java_library_path}"]
+        + (
+            ["-Dgraal.Dump=Truffle:1", "-Dgraal.PrintGraph=Network"]
+            if args.truffle_debug
+            else []
+        )
+        + [
+            f'-Dtruffle.class.path.append={P.join(lkql_jit_home, "lkql_jit.jar")}',
+            "--add-opens=org.graalvm.truffle/com.oracle.truffle.api.strings=ALL-UNNAMED",
+            "com.adacore.lkql_jit.cli.LKQLMain",
+        ]
+        + remaining
+        + ([
+                "--experimental-options",
+                "--engine.Compilation=true",
+                "--engine.TraceCompilation=false",
+                "--engine.TraceCompilationDetails=true",
+                "--engine.TracePerformanceWarnings=all",
+                "--engine.CompilationFailureAction=Diagnose",
+                '--vm.XX:+TraceDeoptimization',
+                '--engine.NodeSourcePositions',
+                '--engine.TraceTransferToInterpreter',
+                '--vm.XX:+TraceDeoptimizationDetails',
+                '--engine.NodeSourcePositions',
+                '--vm.XX:+UnlockDiagnosticVMOptions',
+                '--vm.XX:+DebugNonSafepoints',
+                '--vm.XX:+TraceDeoptimization',
+                '--engine.CompilationStatistics',
+                '--engine.BackgroundCompilation=false',
+                '--engine.TraceDeoptimizeFrame'
+            ]
+            if args.truffle_debug
+            else [])
+        + (['--engine.CompileImmediately=true']
+           if args.compile_immediately else [])
+    )
