604f724bcbd64d1796bb7f25666b9703964c38a9
Hugo Guerrier <guerrier@adacore.com>
2026-02-16 15:47:31 +0100

    Add support for call stack storing in 'Error'

--- lkql_jit/driver/src/main/java/com/adacore/lkql_jit/driver/diagnostics/variants/Error.java
+++ lkql_jit/driver/src/main/java/com/adacore/lkql_jit/driver/diagnostics/variants/Error.java
@@ -6,20 +6,44 @@
 package com.adacore.lkql_jit.driver.diagnostics.variants;
 
 import com.adacore.lkql_jit.driver.source_support.SourceSection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /** This class represents an error diagnostics. */
 public final class Error extends BaseDiagnostic {
 
+    /** Call stack of the error diagnostic, most recent call first. */
+    public final List<CallStackElement> callStack;
+
     // ----- Constructors -----
 
     /** Create a new error diagnostic with a related location. */
     public Error(String message, SourceSection location) {
-        super(message, Optional.of(location));
+        super(message, Optional.ofNullable(location));
+        this.callStack = new ArrayList<>();
     }
 
     /** Create a new error diagnostic without a location. */
     public Error(String message) {
-        super(message, Optional.empty());
+        this(message, null);
     }
+
+    // ----- Instance methods -----
+
+    /** Add a call at the end of the call stack of this error. */
+    public void addCallToStack(String callContext, SourceSection callLocation) {
+        this.callStack.add(new CallStackElement(callContext, callLocation));
+    }
+
+    // ----- Inner classes -----
+
+    /**
+     * This class represents an abstract element of an error call stack.
+     *
+     * @param callContext Name of the context the call has been done (wrapping function, script
+     *                    file name, ...)
+     * @param callLocation Where the call has been made.
+     */
+    public record CallStackElement(String callContext, SourceSection callLocation) {}
 }
