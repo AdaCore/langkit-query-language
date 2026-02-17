28aa19e7d5ba3d401283ce0820ddbbca49c9f3c6
Hugo Guerrier <guerrier@adacore.com>
2026-02-17 15:11:49 +0100

    minor: Do some cleanups in the 'LKQLRun' class

--- lkql_jit/driver/src/main/java/com/adacore/lkql_jit/driver/subcommands/LKQLRun.java
+++ lkql_jit/driver/src/main/java/com/adacore/lkql_jit/driver/subcommands/LKQLRun.java
@@ -28,110 +28,46 @@ import picocli.CommandLine;
 /**
  * This class is the LKQL launcher, this will handle all execution request coming from the command
  * line.
- *
- * @author Hugo GUERRIER
- *     <p>TODO : Support all features of the original LKQL Ada implementation
  */
 public class LKQLRun extends AbstractLanguageLauncher {
 
-    @CommandLine.Command(name = "run", description = "Run the LKQL interpreter on a given script")
-    public static class Args implements Callable<Integer> {
+    // ----- Attributes -----
 
-        @CommandLine.Spec
-        public CommandLine.Model.CommandSpec spec;
+    private final Args args;
 
-        @CommandLine.Parameters(description = "Files to analyze")
-        public List<String> files = new ArrayList<>();
-
-        @CommandLine.Option(
-            names = { "-C", "--charset" },
-            description = "Charset to use for the source decoding"
-        )
-        public String charset = null;
-
-        @CommandLine.Option(names = { "-P", "--project" }, description = "Project file to use")
-        public String project = null;
-
-        @CommandLine.Option(names = "--RTS", description = "Runtime to pass to GPR")
-        public String RTS = null;
-
-        @CommandLine.Option(names = "--target", description = "Hardware target to pass to GPR")
-        public String target = null;
-
-        @CommandLine.Option(
-            names = { "-U", "--recursive" },
-            description = "Process all units in the project tree, excluding externally built" +
-                " projects"
-        )
-        public boolean recursive;
-
-        @CommandLine.Option(names = { "-v", "--verbose" }, description = "Enable the verbose mode")
-        public boolean verbose;
-
-        @CommandLine.Unmatched
-        public List<String> unmatched;
-
-        @CommandLine.Option(
-            names = { "-S", "--script-path" },
-            description = "The LKQL script to execute"
-        )
-        public String script = null;
-
-        @CommandLine.Option(names = { "-i", "--interactive" }, description = "Run a REPL")
-        public boolean interactive;
-
-        @CommandLine.Option(
-            names = "--keep-going-on-missing-file",
-            description = "Keep going on missing file"
-        )
-        public Boolean keepGoingOnMissingFile = false;
-
-        @Override
-        public Integer call() {
-            String[] unmatchedArgs;
-            if (this.unmatched == null) {
-                unmatchedArgs = new String[0];
-            } else {
-                unmatchedArgs = this.unmatched.toArray(new String[0]);
-            }
-            new com.adacore.lkql_jit.driver.subcommands.LKQLRun(this).launch(unmatchedArgs);
-            return 0;
-        }
-    }
+    // ----- Constructors -----
 
     public LKQLRun(Args args) {
         this.args = args;
     }
 
-    private Args args = null;
-
     // ----- Launcher methods -----
 
-    /**
-     * Display the help message for the LKQL language.
-     *
-     * @param maxCategory The option category.
-     */
+    /** Display the help message for the LKQL language. */
     @Override
     protected void printHelp(OptionCategory maxCategory) {
         this.args.spec.commandLine().usage(this.args.spec.commandLine().getOut());
     }
 
-    /**
-     * Simply return the language id.
-     *
-     * @return The language id.
-     */
+    /** Simply return the language id. */
     @Override
     protected String getLanguageId() {
         return Constants.LKQL_ID;
     }
 
     /**
-     * The entry point of the launcher with the context builder.
-     *
-     * @param contextBuilder The context builder.
+     * Parse the command line arguments and return the unrecognized options to parse it with the
+     * default parser.
      */
+    @Override
+    protected List<String> preprocessArguments(
+        List<String> arguments,
+        Map<String, String> polyglotOptions
+    ) {
+        return args.unmatched;
+    }
+
+    /** The entry point of the launcher with the context builder. */
     @Override
     protected void launch(Context.Builder contextBuilder) {
         int exitCode = this.executeScript(contextBuilder);
@@ -140,21 +76,13 @@ public class LKQLRun extends AbstractLanguageLauncher {
         }
     }
 
-    /**
-     * Execute the LKQL script and return the exit code.
-     *
-     * @param contextBuilder The context builder.
-     * @return The exit code of the script.
-     */
+    /** Execute the LKQL script and return the exit code. */
     protected int executeScript(Context.Builder contextBuilder) {
-        // Create the LKQL options object builder
-        final var optionsBuilder = new LKQLOptions.Builder();
-
         // Set the common configuration
         contextBuilder.allowIO(IOAccess.ALL);
 
-        // Forward the command line options to the options object builder
-        optionsBuilder
+        // Forward the command line options to the options builder
+        final var optionsBuilder = new LKQLOptions.Builder()
             .engineMode(LKQLOptions.EngineMode.INTERPRETER)
             .verbose(this.args.verbose)
             .projectFile(this.args.project)
@@ -213,25 +141,64 @@ public class LKQLRun extends AbstractLanguageLauncher {
         }
     }
 
-    // ----- Argument parsing methods -----
+    // ----- Inner classes -----
 
-    /**
-     * Parse the command line arguments and return the unrecognized options to parse it with the
-     * default parser.
-     *
-     * @param arguments The arguments to parse.
-     * @param polyglotOptions The polyglot options.
-     * @return The unrecognized options.
-     */
-    @Override
-    protected List<String> preprocessArguments(
-        List<String> arguments,
-        Map<String, String> polyglotOptions
-    ) {
-        if (this.args.unmatched != null) {
-            return this.args.unmatched;
-        } else {
-            return new ArrayList<>();
+    @CommandLine.Command(name = "run", description = "Run the LKQL interpreter on a given script")
+    public static class Args implements Callable<Integer> {
+
+        @CommandLine.Spec
+        public CommandLine.Model.CommandSpec spec;
+
+        @CommandLine.Parameters(description = "Files to analyze")
+        public List<String> files = new ArrayList<>();
+
+        @CommandLine.Option(
+            names = { "-C", "--charset" },
+            description = "Charset to use for the source decoding"
+        )
+        public String charset = null;
+
+        @CommandLine.Option(names = { "-P", "--project" }, description = "Project file to use")
+        public String project = null;
+
+        @CommandLine.Option(names = "--RTS", description = "Runtime to pass to GPR")
+        public String RTS = null;
+
+        @CommandLine.Option(names = "--target", description = "Hardware target to pass to GPR")
+        public String target = null;
+
+        @CommandLine.Option(
+            names = { "-U", "--recursive" },
+            description = "Process all units in the project tree, excluding externally built" +
+                " projects"
+        )
+        public boolean recursive;
+
+        @CommandLine.Option(names = { "-v", "--verbose" }, description = "Enable the verbose mode")
+        public boolean verbose;
+
+        @CommandLine.Option(
+            names = { "-S", "--script-path" },
+            description = "The LKQL script to execute"
+        )
+        public String script = null;
+
+        @CommandLine.Option(names = { "-i", "--interactive" }, description = "Run a REPL")
+        public boolean interactive;
+
+        @CommandLine.Option(
+            names = "--keep-going-on-missing-file",
+            description = "Keep going on missing file"
+        )
+        public Boolean keepGoingOnMissingFile = false;
+
+        @CommandLine.Unmatched
+        public List<String> unmatched = new ArrayList<>();
+
+        @Override
+        public Integer call() {
+            new LKQLRun(this).launch(unmatched.toArray(new String[0]));
+            return 0;
         }
     }
 }
